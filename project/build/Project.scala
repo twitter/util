c901f9ec944a4fcae1e5a49bfd74c74f277757fe
Nick Kallen <nkallen@tallbro.local>
2010-08-03 12:22:21 -0700

    Publishing to our local maven repo

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,7 @@
 import sbt._
 import com.twitter.sbt._
 
-class UtilProject(info: ProjectInfo) extends StandardProject(info) {
+class UtilProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository {
   val guava = "com.google.guava" % "guava" % "r06"
   val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
