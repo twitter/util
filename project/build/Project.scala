04d0a8f0d789dda39d32f3dc2c8853809ad39835
Jeremy Cloud <jeremycloud@twitter.com>
2010-08-03 18:40:50 -0700

    no-longer StandardProject; publishing to scala0.net for now

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,10 +1,11 @@
 import sbt._
-import com.twitter.sbt._
 
-class UtilProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository {
+class UtilProject(info: ProjectInfo) extends DefaultProject(info) {
+  val publishTo = Resolver.sftp("sftp:scala0.net", "scala0.net", "/var/www/scala0.net/public/repositories")
+
   val guava = "com.google.guava" % "guava" % "r06"
   val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   val mockito = "org.mockito" % "mockito-core" % "1.8.1" % "test"
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3" % "provided"
 }
