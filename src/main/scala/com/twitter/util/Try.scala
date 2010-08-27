970e7431346ec21dc38f23b2d46142853a0b6437
Nick Kallen <nkallen@yahoo.com>
2010-08-26 22:13:47 -0700

    New Try type

---
+++ src/main/scala/com/twitter/util/Try.scala
@@ -0,0 +1,82 @@
+package com.twitter.util
+
+/**
+ * The Try type represents a computation that may either result in an exception
+ * or return a value. It is analogous to the Either type but encodes common idioms
+ * for handling exceptional cases.
+ */
+object Try {
+  def apply[R](r: => R) = {
+    try { Return(r) } catch { case e => Throw(e) }
+  }
+}
+
+sealed abstract class Try[+E <: Throwable, +R] {
+  /**
+   * Returns true if the Try is a Throw, false otherwise.
+   */
+  def isThrow: Boolean
+
+  /**
+   * Returns true if the Try is a Return, false otherwise.
+   */
+  def isReturn: Boolean
+
+  /**
+   * Returns the value from this Return or the given argument if this is a Throw.
+   */
+  def getOrElse[R2 >: R](default: => R2) = if (isReturn) apply() else default
+
+  /**
+   * Calls the exceptionHandler with the exception if this is a Throw.
+   */
+  def rescue[R2 >: R](handleException: E => R2): R2
+
+  /**
+   * Returns the value from this Return or throws the exception if this is a Throw
+   */
+  def apply(): R
+
+  /**
+   * Applies the given function f if this is a Result.
+   */
+  def foreach(f: R => Unit) { if (isReturn) f(apply()) }
+
+  /**
+   * Returns the given function applied to the value from this Return or returns this if this is a Throw
+   */
+  def flatMap[E2 >: E <: Throwable, R2 >: R](f: R => Try[E2, R2]): Try[E2, R2]
+
+  /**
+   * Maps the given function to the value from this Return or returns this if this is a Throw
+   */
+  def map[X](f: R => X): Try[E, X]
+
+  /**
+   * Returns None if this is a Throw or the given predicate does not obtain. Returns some(this) otherwise.
+   */
+  def filter(p: R => Boolean) = if (isReturn && p(apply())) Some(this) else None
+
+  /**
+   * Returns None if this is a Throw or a Some containing the value if this is a Return
+   */
+  def toOption = if (isReturn) Some(apply()) else None
+}
+
+final case class Throw[+E <: Throwable, +R](e: E) extends Try[E, R] { 
+  def isThrow = true
+  def isReturn = false
+  def rescue[R2 >: R](handleException: E => R2) = handleException(e)
+  def apply(): R = throw e
+  def flatMap[E2 >: E <: Throwable, R2 >: R](f: R => Try[E2, R2]) = Throw[E2, R](e)
+  def map[X](f: R => X) = Throw(e)
+}
+
+final case class Return[+E <: Throwable, +R](r: R) extends Try[E, R] {
+  def isThrow = false
+  def isReturn = true
+  def rescue[R2 >: R](handleException: E => R2) = r
+  def apply() = r
+  def flatMap[E2 >: E <: Throwable, R2 >: R](f: R => Try[E2, R2]) = f(r)
+  def map[X](f: R => X) = Return(f(r))
+}
---
+++ src/test/scala/com/twitter/util/TrySpec.scala
@@ -0,0 +1,73 @@
+package com.twitter.util
+
+import org.specs.Specification
+
+object TrySpec extends Specification {
+  val e = new Exception
+
+  "Try()" should {
+    "catch exceptions and lift into the Try type" in {
+      Try(1) mustEqual Return(1)
+      Try { throw e } mustEqual Throw(e)
+    }
+  }
+
+  "Try" should {
+    "rescue" in {
+      Return(1) rescue { case _ => 2 } mustEqual 1
+      Throw(e) rescue { case _ => 2 } mustEqual 2
+    }
+
+    "getOrElse" in {
+      Return(1) getOrElse 2 mustEqual 1
+      Throw(e) getOrElse 2 mustEqual 2
+    }
+
+    "apply" in {
+      Return(1)() mustEqual 1
+      Throw[Exception, Int](e)() must throwA(e)
+    }
+
+    "map" in {
+      Return(1) map(1+) mustEqual Return(2)
+      Throw[Exception, Int](e) map(1+) mustEqual Throw(e)
+    }
+
+    "for" in {
+      "with no Throw values" in {
+        val result = for {
+          i <- Return(1)
+          j <- Return(1)
+        } yield (i + j)
+        result mustEqual Return(2)
+      }
+
+      "with Throw values" in {
+        "throws before" in {
+          val result = for {
+            i <- Throw[Exception, Int](e)
+            j <- Return(1)
+          } yield (i + j)
+          result mustEqual Throw(e)
+        }
+
+        "throws after" in {
+          val result = for {
+            i <- Return(1)
+            j <- Throw[Exception, Int](e)
+          } yield (i + j)
+          result mustEqual Throw(e)
+        }
+
+        "returns the FIRST Throw" in {
+          val e2 = new Exception
+          val result = for {
+            i <- Throw[Exception, Int](e)
+            j <- Throw[Exception, Int](e2)
+          } yield (i + j)
+          result mustEqual Throw(e)
+        }
+      }
+    }
+  }
+}
