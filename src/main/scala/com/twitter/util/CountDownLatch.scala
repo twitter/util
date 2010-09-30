877b5c4723806987b30913ed94e4b1c83f883990
Jeremy Cloud <jeremycloud@twitter.com>
2010-09-30 09:09:59 -0700

    added current count getter and isZero method

--- src/main/scala/com/twitter/util/CountDownLatch.scala
+++ src/main/scala/com/twitter/util/CountDownLatch.scala
@@ -2,8 +2,10 @@ package com.twitter.util
 
 import java.util.concurrent.TimeUnit
 
-class CountDownLatch(val count: Int) {
-  val underlying = new java.util.concurrent.CountDownLatch(count)
+class CountDownLatch(val initialCount: Int) {
+  val underlying = new java.util.concurrent.CountDownLatch(initialCount)
+  def count = underlying.getCount
+  def isZero = count == 0
   def countDown() = underlying.countDown()
   def await() = underlying.await()
   def await(timeout: Duration) = underlying.await(timeout.inMillis, TimeUnit.MILLISECONDS)
