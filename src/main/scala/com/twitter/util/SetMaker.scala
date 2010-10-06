9b3647df17e88bdf2420696bfe292d682de55500
Nick Kallen <nkallen@tallbro.local>
2010-10-05 21:11:53 -0700

    integrated jeremy's hashsetness

--- src/main/scala/com/twitter/util/SetMaker.scala
+++ src/main/scala/com/twitter/util/SetMaker.scala
@@ -2,38 +2,41 @@ package com.twitter.util
 
 import scala.collection.mutable.{Map, Set}
 import java.util.concurrent.TimeUnit
-import collection.JavaConversions._
+import collection.JavaConversions.JConcurrentMapWrapper
 import com.google.common.collect.{MapMaker => GoogleMapMaker}
 import com.google.common.base.Function
-// 
-// object SetMaker {
-//   def apply[A](f: Config[A] => Any): Set[A] = {
-//     val config = new Config[A]
-//     f(config)
-//     config()
-//   }
-// 
-//   class Config[A] {
-//     private val mapMaker = new GoogleMapMaker
-// 
-//     def weakValues = { mapMaker.weakKeys; mapMaker.weakValues; this }
-//     def softValues = { mapMaker.softKeys; mapMaker.softValues; this }
-//     def concurrencyLevel(level: Int) = { mapMaker.concurrencyLevel(level); this }
-//     def initialCapacity(capacity: Int) = { mapMaker.initialCapacity(capacity); this }
-//     def expiration(expiration: Duration) = { mapMaker.expiration(expiration.inMillis, TimeUnit.MILLISECONDS); this }
-// 
-//     def apply() = mapMaker.makeMap()
-//   }
-// }
-// 
-// object MapToSet {
-//   def apply[A](map: Map[A, A]): Set[A] = new MapToSetAdapter(map)
-// }
-// 
-// class MapToSetAdapter[A](map: Map[A,A]) extends Set[A] {
-//   def +=(elem: A) = map(elem) = elem
-//   def -=(elem: A) = map -= elem
-//   def size = map.size
-//   def elements = map.keys
-//   def contains(elem: A) = map.contains(elem)
-// }
+
+object SetMaker {
+  def apply[A](f: Config[A] => Any): Set[A] = {
+    val config = new Config[A]
+    f(config)
+    config()
+  }
+
+  class Config[A] {
+    private val mapMaker = new GoogleMapMaker
+
+    def weakValues = { mapMaker.weakKeys; mapMaker.weakValues; this }
+    def softValues = { mapMaker.softKeys; mapMaker.softValues; this }
+    def concurrencyLevel(level: Int) = { mapMaker.concurrencyLevel(level); this }
+    def initialCapacity(capacity: Int) = { mapMaker.initialCapacity(capacity); this }
+    def expiration(expiration: Duration) = { mapMaker.expiration(expiration.inMillis, TimeUnit.MILLISECONDS); this }
+
+    def apply() = new MapToSetAdapter(
+      new JConcurrentMapWrapper[A, A](mapMaker.makeMap()))
+  }
+}
+
+class MapToSetAdapter[A](map: Map[A, A]) extends Set[A] {
+  def +=(elem: A) = {
+    map(elem) = elem
+    this
+  }
+  def -=(elem: A) = {
+    map -= elem
+    this
+  }
+  override def size = map.size
+  def iterator = map.keysIterator
+  def contains(elem: A) = map.contains(elem)
+}
