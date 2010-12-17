d0e12a2cdbb0300ff6056a5827e686716f0ad1d8
Robey Pointer <robey@twitter.com>
2010-12-16 18:50:55 -0800

    simplify this test.

--- src/test/scala/com/twitter/util/EvaluatorSpec.scala
+++ src/test/scala/com/twitter/util/EvaluatorSpec.scala
@@ -5,17 +5,7 @@ import java.io.{File, FileOutputStream}
 
 object EvaluatorSpec extends Specification {
   def file(path: String) = {
-    val stream = getClass.getResourceAsStream(path)
-    val file = File.createTempFile(path, "scala")
-    val fos = new FileOutputStream(file)
-
-    var byte = stream.read()
-    while (byte != -1) {
-      fos.write(byte)
-      byte = stream.read()
-    }
-
-    file
+    new File("src/test/resources", path)
   }
 
   "Evaluator" should {
@@ -24,7 +14,7 @@ object EvaluatorSpec extends Specification {
     }
 
     "apply(new File(...))" in {
-      Eval[Int](new File("src/test/resources/OnePlusOne.scala")) mustEqual 2
+      Eval[Int](file("OnePlusOne.scala")) mustEqual 2
     }
 
     "apply(new File(...), new File(...))" in {
