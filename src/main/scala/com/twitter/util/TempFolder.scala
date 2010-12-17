8e6a0bfd943c54cbbab99909252273b0a42e08b4
Robey Pointer <robey@twitter.com>
2010-12-17 10:37:49 -0800

    port over TempFolder.

---
+++ src/main/scala/com/twitter/util/TempFolder.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2010 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.util
+
+import java.io.File
+
+/**
+ * Test mixin that creates a new folder for a block of code to execute in.
+ * The folder is recursively deleted after the test.
+ */
+trait TempFolder {
+  private val _folderName = new ThreadLocal[File]
+
+  // recursively delete a folder. should be built in. bad java.
+  private def deleteFolder(folder: File): Unit = {
+    folder.listFiles.foreach { f =>
+      if (f.isDirectory) {
+        deleteFolder(f)
+      } else {
+        f.delete
+      }
+    }
+    folder.delete
+  }
+
+  def withTempFolder(f: => Any): Unit = {
+    val tempFolder = System.getProperty("java.io.tmpdir")
+    var folder: File = null
+    do {
+      folder = new File(tempFolder, "scala-test-" + System.currentTimeMillis)
+    } while (! folder.mkdir)
+    _folderName.set(folder)
+
+    try {
+      f
+    } finally {
+      deleteFolder(folder)
+    }
+  }
+
+  def folderName = { _folderName.get.getPath }
+}
