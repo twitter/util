9e1141dc317793dfb1bdf8e679737ae880e3af8a
Nick Kallen <nkallen@tallbro.local>
2011-01-11 13:23:49 -0800

    new version

--- src/main/scala/com/twitter/util/Timer.scala
+++ src/main/scala/com/twitter/util/Timer.scala
@@ -1,10 +1,14 @@
 package com.twitter.util
 
-trait Timer {
-  def schedule(when: Time)(f: => Unit)
-  def schedule(when: Time, period: Duration)(f: => Unit)
+trait TimerTask {
+  def cancel()
+}
 
-  def schedule(period: Duration)(f: => Unit) {
+trait Timer {
+  def schedule(when: Time)(f: => Unit): TimerTask
+  def schedule(when: Time, period: Duration)(f: => Unit): TimerTask
+
+  def schedule(period: Duration)(f: => Unit): TimerTask = {
     schedule(period.fromNow, period)(f)
   }
 
@@ -14,21 +18,27 @@ trait Timer {
 class JavaTimer(isDaemon: Boolean) extends Timer {
   def this() = this(false)
 
-  val javaTimer = new java.util.Timer(isDaemon)
+  private[this] val underlying = new java.util.Timer(isDaemon)
 
-  def schedule(when: Time)(f: => Unit) {
-    javaTimer.schedule(toTask(f), when.toDate)
+  def schedule(when: Time)(f: => Unit) = {
+    val task = toJavaTimerTask(f)
+    underlying.schedule(task, when.toDate)
+    toTimerTask(task)
   }
 
-  def schedule(when: Time, period: Duration)(f: => Unit) {
-    javaTimer.schedule(toTask(f), when.toDate, period.inMillis)
+  def schedule(when: Time, period: Duration)(f: => Unit) = {
+    val task = toJavaTimerTask(f)
+    underlying.schedule(task, when.toDate, period.inMillis)
+    toTimerTask(task)
   }
 
-  def stop() = javaTimer.cancel()
+  def stop() = underlying.cancel()
 
-  private[this] def toTask(f: => Unit) = new java.util.TimerTask {
-    def run {
-      f
-    }
+  private[this] def toJavaTimerTask(f: => Unit) = new java.util.TimerTask {
+    def run { f }
+  }
+
+  private[this] def toTimerTask(task: java.util.TimerTask) = new TimerTask {
+    def cancel() { task.cancel() }
   }
 }
