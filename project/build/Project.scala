9f9e2c8db6bd992b73323a3b8e272b351bca8148
Matt Freels <matt@freels.name>
2011-03-02 12:23:30 -0800

    pull out eval

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -15,6 +15,10 @@ class Project(info: ProjectInfo) extends StandardParentProject(info) with Subver
     "util-core", "util",
     new CoreProject(_))
 
+  val evalProject = project(
+    "util-eval", "util-eval",
+    new EvalProject(_), coreProject)
+
   val collectionProject = project(
     "util-collection", "util-collection",
     new CollectionProject(_), coreProject)
@@ -25,27 +29,23 @@ class Project(info: ProjectInfo) extends StandardParentProject(info) with Subver
     new ReflectProject(_), coreProject)
 
 
-  class CoreProject(info: ProjectInfo) extends StandardProject(info)
-    with ProjectDefaults with SubversionPublisher
-  {
+  class CoreProject(info: ProjectInfo) extends StandardProject(info) with ProjectDefaults
+
+  class EvalProject(info: ProjectInfo) extends StandardProject(info) with ProjectDefaults {
     val scalaTools = "org.scala-lang" % "scala-compiler" % "2.8.1" % "compile"
     override def filterScalaJars = false
   }
 
-  class CollectionProject(info: ProjectInfo) extends StandardProject(info)
-    with ProjectDefaults with SubversionPublisher
-  {
+  class CollectionProject(info: ProjectInfo) extends StandardProject(info) with ProjectDefaults {
     val guava              = "com.google.guava"    % "guava"               % "r06"
     val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
   }
 
-  class ReflectProject(info: ProjectInfo) extends StandardProject(info)
-    with ProjectDefaults with SubversionPublisher
-  {
+  class ReflectProject(info: ProjectInfo) extends StandardProject(info) with ProjectDefaults {
     val cglib = "cglib" % "cglib" % "2.2"
   }
 
-  trait ProjectDefaults extends StandardProject {
+  trait ProjectDefaults extends StandardProject with SubversionPublisher {
     val specs   = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test" withSources()
     val mockito = "org.mockito"             % "mockito-all" % "1.8.5" % "test" withSources()
     val junit   = "junit"                   %       "junit" % "3.8.2" % "test"
