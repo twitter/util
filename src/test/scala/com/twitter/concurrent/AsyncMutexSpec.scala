f80e6e0540d7a55fde94e41adf563a0598c61598
Nick Kallen <nkallen@tallbro.local>
2011-03-03 14:25:59 -0800

    Importing AsyncSemaphore from finagle, rewriting Mutex in terms of it. This changes AsyncMutex interface slightly. Instead of returning Future[() => Unit] it returns a Future[Permit]

--- src/test/scala/com/twitter/concurrent/AsyncMutexSpec.scala
+++ src/test/scala/com/twitter/concurrent/AsyncMutexSpec.scala
@@ -3,7 +3,7 @@ package com.twitter.concurrent
 import org.specs.Specification
 
 object AsyncMutexSpec extends Specification {
-  "AsyncMutext" should {
+  "AsyncMutex" should {
     "admit only one operation at a time" in {
       val m = new AsyncMutex
 
@@ -13,12 +13,12 @@ object AsyncMutexSpec extends Specification {
       a0.isDefined must beTrue
       a1.isDefined must beFalse
 
-      a0()()                     // satisfy operation 0
+      a0().release()             // satisfy operation 0
       a1.isDefined must beTrue   // 1 now available
 
       val a2 = m.acquire()
       a2.isDefined must beFalse
-      a1()()
+      a1().release()             // satisfy operation 1
       a2.isDefined must beTrue
     }
   }
