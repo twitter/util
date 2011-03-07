7fc11e5e2c2d891a04e899e8f42f98d9bb3f83d2
Oliver Gould <github@olix0r.net>
2011-03-07 15:12:00 -0800

    StringEncoder isn't a useful API for all Thrift encoding. JsonThriftSerializer.toString is provided as a convenience. The Base64 Serializer has been removed since it does not apply to thrift objects.

--- util-thrift/src/main/scala/com/twitter/util/ThriftSerializer.scala
+++ util-thrift/src/main/scala/com/twitter/util/ThriftSerializer.scala
@@ -7,12 +7,7 @@ import org.apache.thrift.protocol.{TBinaryProtocol, TCompactProtocol, TProtocolF
   TSimpleJSONProtocol}
 import org.apache.thrift.transport.TIOStreamTransport
 
-trait StringEncoder {
-  def encode(bytes: Array[Byte]): String
-  def decode(str: String): Array[Byte]
-}
-
-trait ThriftSerializer extends StringEncoder {
+trait ThriftSerializer {
   def protocolFactory: TProtocolFactory
 
   def toBytes(obj: TBase[_, _]): Array[Byte] = {
@@ -23,22 +18,17 @@ trait ThriftSerializer extends StringEncoder {
 
   def fromBytes(obj: TBase[_, _], bytes: Array[Byte]): Unit =
     obj.read(protocolFactory.getProtocol(new TIOStreamTransport(new ByteArrayInputStream(bytes))))
-
-  def toString(obj: TBase[_, _]): String = encode(toBytes(obj))
-
-  def fromString(obj: TBase[_, _], str: String): Unit = fromBytes(obj, decode(str))
-
-  override def encode(bytes: Array[Byte]): String = new String(bytes)
-  override def decode(str: String): Array[Byte] = str.getBytes
 }
 
 class JsonThriftSerializer extends ThriftSerializer {
   override def protocolFactory = new TSimpleJSONProtocol.Factory
 
+  def toString(obj: TBase[_, _]): String = new String(toBytes(obj))
+
   /**
    * Thrift does not properly deserialize the JSON it serializes ;/
    */
-  override def fromString(obj: TBase[_, _], str: String): Unit =
+  def fromString(obj: TBase[_, _], str: String): Unit =
     throw new UnsupportedOperationException("Thrift does not properly deserialize the JSON")
 
   /**
@@ -54,9 +44,4 @@ class BinaryThriftSerializer extends ThriftSerializer {
 
 class CompactThriftSerializer extends ThriftSerializer {
   override def protocolFactory = new TCompactProtocol.Factory
-}
-
-trait Base64StringEncoder extends StringEncoder {
-  override def encode(bytes: Array[Byte]) = new String(Base64.encodeBase64(bytes))
-  override def decode(str: String) = Base64.decodeBase64(str.getBytes)
 }
