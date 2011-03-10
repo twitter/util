9688bb12a93473d3dfabe2079a1540b0a2010f90
Oliver Gould <ver@twitter.com>
2011-03-10 13:02:28 -0800

    Implement a simple counter-incrementing test for the ScheduledPoolTimer

--- util-core/src/test/scala/com/twitter/util/TimerSpec.scala
+++ util-core/src/test/scala/com/twitter/util/TimerSpec.scala
@@ -1,5 +1,7 @@
 package com.twitter.util
 
+import java.util.concurrent.atomic.AtomicInteger
+import com.twitter.conversions.time._
 import org.specs.Specification
 import org.specs.mock.Mockito
 
@@ -37,6 +39,17 @@ object TimerSpec extends Specification with Mockito {
       val timer = new ScheduledPoolTimer(1)
       timer must notBeNull
       timer.stop()
+    }
+
+    "increment a counter" in {
+      val timer = new ScheduledPoolTimer(1)
+      val counter = new AtomicInteger(0)
+      timer.schedule(0.millis, 20.millis) {
+        counter.incrementAndGet()
+      }
+      Thread.sleep(1.second.inMillis)
+      timer.stop()
+      counter.get() must beCloseTo(50, 1)
     }
   }
 }
