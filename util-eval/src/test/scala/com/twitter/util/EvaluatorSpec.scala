d031b6022d252b84d683e4bbac752f459c9ce60f
marius a. eriksen <marius@twitter.com>
2011-03-31 15:29:52 -0700

    [bc] util: fix EvaluatorSpec to be path indepedent.

--- util-eval/src/test/scala/com/twitter/util/EvaluatorSpec.scala
+++ util-eval/src/test/scala/com/twitter/util/EvaluatorSpec.scala
@@ -3,22 +3,22 @@ package com.twitter.util
 import org.specs.Specification
 import java.io.{File, FileOutputStream}
 
-object EvaluatorSpec extends Specification {
-  def file(path: String) = {
-    new File("util-eval/src/test/resources", path)
-  }
+import com.twitter.io.TempFile
 
+object EvaluatorSpec extends Specification {
   "Evaluator" should {
     "apply('expression')" in {
       Eval[Int]("1 + 1") mustEqual 2
     }
 
     "apply(new File(...))" in {
-      Eval[Int](file("OnePlusOne.scala")) mustEqual 2
+      Eval[Int](TempFile.fromResourcePath("/OnePlusOne.scala")) mustEqual 2
     }
 
     "apply(new File(...), new File(...))" in {
-      val derived = Eval[() => String](file("/Base.scala"), file("/Derived.scala"))
+      val derived = Eval[() => String](
+        TempFile.fromResourcePath("/Base.scala"),
+        TempFile.fromResourcePath("/Derived.scala"))
       derived() mustEqual "hello"
     }
 
