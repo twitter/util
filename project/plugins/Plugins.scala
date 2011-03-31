9e47a39bbcd7d12870f9c9990b66dd2689318520
marius a. eriksen <marius@twitter.com>
2011-03-31 14:53:33 -0700

    [split] published version of standard-project.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,5 +5,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
   val twitterMaven = "twitter.com" at "http://maven.twttr.com/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.11.16-SNAPSHOT"
+  val defaultProject = "com.twitter" % "standard-project" % "0.11.16-NEST"
 }
