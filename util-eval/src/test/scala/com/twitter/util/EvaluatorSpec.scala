4a38622f8491fa11207b056eaf84a3ba3d210000
Robey Pointer <robey@twitter.com>
2011-03-31 18:49:34 -0700

    allow in-place eval of the type scrooge would like to do.s

--- util-eval/src/test/scala/com/twitter/util/EvaluatorSpec.scala
+++ util-eval/src/test/scala/com/twitter/util/EvaluatorSpec.scala
@@ -8,7 +8,7 @@ object EvaluatorSpec extends Specification {
     new File("util-eval/src/test/resources", path)
   }
 
-  "Evaluator" should {
+  "Eval" should {
     "apply('expression')" in {
       Eval[Int]("1 + 1") mustEqual 2
     }
@@ -24,6 +24,12 @@ object EvaluatorSpec extends Specification {
 
     "apply(InputStream)" in {
       Eval[Int](getClass.getResourceAsStream("/OnePlusOne.scala")) mustEqual 2
+    }
+
+    "inPlace('expression')" in {
+      Eval.compile("object Doubler { def apply(n: Int) = n * 2 }")
+      Eval.inPlace[Int]("Doubler(2)") mustEqual 4
+      Eval.inPlace[Int]("Doubler(14)") mustEqual 28
     }
   }
 }
