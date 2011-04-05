971967fd769159eab47053d5e7a681faeb61abac
marius a. eriksen <marius@twitter.com>
2011-04-05 14:54:56 -0700

    [bc] 0.11.24-NEST

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -15,5 +15,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   }
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
-  val defaultProject = "com.twitter" % "standard-project" % "0.11.23-NEST"
+  val defaultProject = "com.twitter" % "standard-project" % "0.11.24-NEST"
 }
