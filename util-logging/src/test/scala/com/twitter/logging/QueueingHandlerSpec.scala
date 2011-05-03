9d8a1a5b33a7353911b1a60a263035555a69f381
David Helder <david@twitter.com>
2011-05-03 13:37:49 -0700

    [split] Put eventually LHS in a block so it can be re-evaluated

--- util-logging/src/test/scala/com/twitter/logging/QueueingHandlerSpec.scala
+++ util-logging/src/test/scala/com/twitter/logging/QueueingHandlerSpec.scala
@@ -67,7 +67,7 @@ class QueueingHandlerSpec extends Specification {
       logger.warning("1")
       logger.warning("2")
       logger.warning("3")
-      droppedCount must eventually(10, 1.second)(be_==(2))
+      (() => droppedCount)() must eventually(be_==(2))
     }
 
     "flush" in {
