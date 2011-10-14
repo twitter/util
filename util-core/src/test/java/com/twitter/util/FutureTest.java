16c28b93558d2d083509426a5d0579cd2c4dbeb9
Robey Pointer <robey@twitter.com>
2011-10-14 11:56:22 -0700

    [split] shortcut for statics

--- util-core/src/test/java/com/twitter/util/FutureTest.java
+++ util-core/src/test/java/com/twitter/util/FutureTest.java
@@ -8,7 +8,7 @@ import junit.framework.TestCase;
  */
 public class FutureTest extends TestCase {
   public void testFutureCastMap() {
-    Future<String> f = Future$.MODULE$.value("23");
+    Future<String> f = Future.value("23");
     Future<Integer> f2 = f.map(new Function<String, Integer>() {
       public Integer apply(String in) {
         return Integer.parseInt(in);
@@ -18,7 +18,7 @@ public class FutureTest extends TestCase {
   }
 
   public void testFutureCastFlatMap() {
-    Future<String> f = Future$.MODULE$.value("23");
+    Future<String> f = Future.value("23");
     Future<Integer> f2 = f.flatMap(new Function<String, Future<Integer>>() {
       public Future<Integer> apply(String in) {
         return Future$.MODULE$.value(Integer.parseInt(in));
