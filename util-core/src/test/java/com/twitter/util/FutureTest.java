5de8a5eca368b0be0bfb70c8a4d28b59aa2eb51c
Robey Pointer <robey@twitter.com>
2011-10-13 17:49:32 -0700

    [split] add another test for flatMap, just in case

--- util-core/src/test/java/com/twitter/util/FutureTest.java
+++ util-core/src/test/java/com/twitter/util/FutureTest.java
@@ -7,11 +7,21 @@ import junit.framework.TestCase;
  * a success.
  */
 public class FutureTest extends TestCase {
-  public void testFutureCast() {
+  public void testFutureCastMap() {
     Future<String> f = Future$.MODULE$.value("23");
     Future<Integer> f2 = f.map(new Function<String, Integer>() {
       public Integer apply(String in) {
         return Integer.parseInt(in);
+      }
+    });
+    assertEquals((int) f2.get(), 23);
+  }
+
+  public void testFutureCastFlatMap() {
+    Future<String> f = Future$.MODULE$.value("23");
+    Future<Integer> f2 = f.flatMap(new Function<String, Future<Integer>>() {
+      public Future<Integer> apply(String in) {
+        return Future$.MODULE$.value(Integer.parseInt(in));
       }
     });
     assertEquals((int) f2.get(), 23);
