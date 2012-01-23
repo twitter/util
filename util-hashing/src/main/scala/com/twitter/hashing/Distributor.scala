c68ad885ce76c20f9e9df7d0cf10339316067b59
Arya Asemanfar <arya@twitter.com>
2012-01-23 15:32:55 -0800

    [split] finagle-memcached: handle all nodes marked dead

--- util-hashing/src/main/scala/com/twitter/hashing/Distributor.scala
+++ util-hashing/src/main/scala/com/twitter/hashing/Distributor.scala
@@ -1,7 +1,13 @@
 package com.twitter.hashing
 
 trait Distributor[A] {
-  def nodeForHash(Hash: Long): A
+  def nodeForHash(hash: Long): A
   def nodeCount: Int
   def nodes: Seq[A]
+}
+
+class SingletonDistributor[A](node: A) extends Distributor[A] {
+  def nodeForHash(hash: Long) = node
+  def nodeCount = 1
+  def nodes = Seq(node)
 }
