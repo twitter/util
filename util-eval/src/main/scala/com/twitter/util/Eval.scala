540806195cdbb7891ad0319610f1a9edf12c1010
Steve Jenson <stevej@twitter.com>
2012-04-04 12:56:01 -0700

    [split]  - @throw is not a scaladoc tag - this removes several warnings

--- util-eval/src/main/scala/com/twitter/util/Eval.scala
+++ util-eval/src/main/scala/com/twitter/util/Eval.scala
@@ -231,7 +231,7 @@ class Eval(target: Option[File]) {
 
   /**
    * Check if code is Eval-able.
-   * @throw CompilerException if not Eval-able.
+   * @throws CompilerException if not Eval-able.
    */
   def check(code: String) {
     val id = uniqueId(sourceForString(code))
@@ -242,7 +242,7 @@ class Eval(target: Option[File]) {
 
   /**
    * Check if files are Eval-able.
-   * @throw CompilerException if not Eval-able.
+   * @throws CompilerException if not Eval-able.
    */
   def check(files: File*) {
     val code = files.map { scala.io.Source.fromFile(_).mkString }.mkString("\n")
@@ -251,7 +251,7 @@ class Eval(target: Option[File]) {
 
   /**
    * Check if stream is Eval-able.
-   * @throw CompilerException if not Eval-able.
+   * @throws CompilerException if not Eval-able.
    */
   def check(stream: InputStream) {
     check(scala.io.Source.fromInputStream(stream).mkString)
