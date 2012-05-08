2c16019d56da56dd2e37db8a8f3fc21c32e4e702
Robey Pointer <robey@twitter.com>
2012-05-07 18:00:17 -0700

    [split] merge stephenjudkins fix for spaces in classpaths.

--- util-eval/src/main/scala/com/twitter/util/Eval.scala
+++ util-eval/src/main/scala/com/twitter/util/Eval.scala
@@ -309,8 +309,12 @@ class Eval(target: Option[File]) {
    * This is probably fragile.
    */
   lazy val impliedClassPath: List[String] = {
-    val currentClassPath = this.getClass.getClassLoader.asInstanceOf[URLClassLoader].getURLs.
-      map(_.toString).filter(_.startsWith("file:")).map(_.substring(5)).toList
+    val loader = this.getClass.getClassLoader.asInstanceOf[URLClassLoader]
+    val currentClassPath = loader.getURLs filter {
+      _.getProtocol == "file"
+    } map { u =>
+      new File(u.toURI).getPath
+    } toList
 
     // if there's just one thing in the classpath, and it's a jar, assume an executable jar.
     currentClassPath ::: (if (currentClassPath.size == 1 && currentClassPath(0).endsWith(".jar")) {
