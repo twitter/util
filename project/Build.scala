8877197f9c91b54e923ad60fe8e89a4f955f3e62
Steve Gury <stevegury@twitter.com>
2014-04-18 21:31:08 +0000

    [split] util: Add a new sbt target to publish locally to m2
    Problem
    
    Publishing the maven-scrooge-plugin requires having access to the latest util.
    Every time we publish, we need to wait for the util release to be available on Sonatype.
    
    Solution
    
    Rather than waiting for the release to be available on Sonatype, this new sbt target
    installs it locally.
    
    Result
    
    ./sbt +publish-m2 publish the jars in the local maven repository
    
    RB_ID=337145

--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,18 @@ object Util extends Build {
     ExclusionRule("javax.jms", "jms")
   )
 
+  lazy val publishM2Configuration =
+    TaskKey[PublishConfiguration]("publish-m2-configuration",
+      "Configuration for publishing to the .m2 repository.")
+
+  lazy val publishM2 =
+    TaskKey[Unit]("publish-m2",
+      "Publishes artifacts to the .m2 repository.")
+
+  lazy val m2Repo =
+    Resolver.file("publish-m2-local",
+      Path.userHome / ".m2" / "repository")
+
   val sharedSettings = Seq(
     version := libVersion,
     organization := "com.twitter",
@@ -29,6 +41,12 @@ object Util extends Build {
     ),
 
     resolvers += "twitter repo" at "http://maven.twttr.com",
+
+    publishM2Configuration <<= (packagedArtifacts, checksums in publish, ivyLoggingLevel) map { (arts, cs, level) =>
+      Classpaths.publishConfig(arts, None, resolverName = m2Repo.name, checksums = cs, logging = level)
+    },
+    publishM2 <<= Classpaths.publishTask(publishM2Configuration, deliverLocal),
+    otherResolvers += m2Repo,
 
     scalacOptions ++= Seq("-encoding", "utf8"),
     scalacOptions += "-deprecation",
