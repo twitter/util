789bc843a9212aef90745cc3871dd0214783aa83
Moses Nakamura <mnakamura@twitter.com>
2014-08-22 16:56:12 -0400

    [split] bump util to 6.20.1-SNAPSHOT
    RB_ID=436989
    TBR=true
    NO-QUEUE=true

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt.Keys._
 import sbt._
 
 object Util extends Build {
-  val libVersion = "6.19.0"
+  val libVersion = "6.20.0"
   val zkVersion = "3.3.4"
   val zkDependency = "org.apache.zookeeper" % "zookeeper" % zkVersion excludeAll(
     ExclusionRule("com.sun.jdmk", "jmxtools"),
