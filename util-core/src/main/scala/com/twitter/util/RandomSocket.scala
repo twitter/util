8160c1fdd087214e3a6a17a7c3c4f89232ee0322
Oliver Gould <ver@twitter.com>
2014-11-13 20:15:56 +0000

    [split] Deprecate RandomSocket
    Problem:
    
    RandomSocket doesn't provide any availability guarantees.
    
    Solution:
    
    Deprecate RandomSocket. Fix com.twitter.finagle.ResolutionRaceTest to not use it.
    
    RB_ID=512581

--- util-core/src/main/scala/com/twitter/util/RandomSocket.scala
+++ util-core/src/main/scala/com/twitter/util/RandomSocket.scala
@@ -12,11 +12,14 @@ object RandomSocket {
     new InetSocketAddress(port)
   private[this] val ephemeralSocketAddress = localSocketOnPort(0)
 
+  @deprecated("RandomSocket cannot ensure that the address is not in use.", "2014-11-13")
   def apply() = nextAddress()
 
+  @deprecated("RandomSocket cannot ensure that the address is not in use.", "2014-11-13")
   def nextAddress(): InetSocketAddress =
     localSocketOnPort(nextPort())
 
+  @deprecated("RandomSocket cannot ensure that the address is not in use.", "2014-11-13")
   def nextPort(): Int = {
     val s = new Socket
     s.setReuseAddress(true)
