fe8cfaa7d02f4ff09d2ebd06d6e2ba0c77e239e8
Ruben Oanta <roanta@twitter.com>
2014-11-21 05:21:22 +0000

    [split] twitter-server: Refactor constructs around /admin
    Problem
    
    Our work towards improving the twitter-server administrator interface was
    incomplete and needed a second pass.
    
    Solution
    
    Make the following improvements:
    
    - Introduce a more flexible mechanism for hooking into the navigation
      pane. The com.twitter.server.Admin trait now defines a `routes` field which
      represents a collection of endpoints that the admin server will add to its
      namespace. This is combined with the global com.twitter.finagle.http.HttpMuxer
      to form the admin http Service.
    
    - Formalize the constructs we use to build admin http endpoints. Each endpoint
      now simply extends Service[Req, Rep] and uses a collection of HttpUtils to do
      http. Mustache templating is replaced in favor of string interpolation.  This
      simplifies our dependencies and gives us more static guarantees. We could take
      this even further and define a custom interpolation that verifies xhtml, for
      example.  Finally, endpoints can be decorated with views (finagle filters)
      which wrap the returned content in an html template. Admin services
      communicate to these views via the http content-type headers.
    
    - Better integration with Metrics. We now more thoroughly search the metric
      namespace for finagle servers and clients. The features which are dependent on
      stats are less brittle since we use the StackRegistry to extract registered
      entries for both servers and clients.
    
    - Simplify how we generate the Summary page. Each endpoint is responsible for
      producing html fragments which are stitched together via ajax calls. This moves
      all the feature logic into Scala rather than Javascript.
    
    - Better integration with Finagle. Client/Server parameter introspection was
      fixed and we now we display the Finagle version in Summary.
    
    - Proper static resource loading via path names rather than http query strings.
      More robust handling of http content-type for common file types.
    
    - Lots of UI fixes and improvements.
    
    RB_ID=501201
    TBR=true

--- util-stats/src/main/scala/com/twitter/finagle/stats/StatsRegistry.scala
+++ util-stats/src/main/scala/com/twitter/finagle/stats/StatsRegistry.scala
@@ -1,21 +1,21 @@
 package com.twitter.finagle.stats
 
 /**
- * Interface used via the LoadService mechanism to obtain a stats
- * registry used in MetricsRegistry. This avoids MetricsHandler,
- * (which uses a stats registry), in twitter-server, having a
- * dependency on finagle-stats.
+ * Interface used via the LoadService mechanism to obtain an
+ * efficient mechanism to sample stats.
  */
 private[twitter] trait StatsRegistry {
-  def getStats(): Map[String, StatEntry]
+  def apply(): Map[String, StatEntry]
 }
 
-/**
- * Interface to allow MetricsHandler (in twitter-server) to
- * use metrics from MetricsRegistry (in finagle-stats)
- * without twitter-server having a dependency on finagle-stats.
- */
 private[twitter] trait StatEntry {
+  /**
+   * The delta since the entry was last sampled.
+   * Note, this field is identical to `value` for
+   * instantaneous entries (ex. gauges).
+   */
+  val delta: Double
+
+  /** The instantaneous value of the entry. */
   val value: Double
-  val totalValue: Double
 }
