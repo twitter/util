63641149f20264492f8dd229bbbb31ac495808f1
Moses Nakamura <mnakamura@twitter.com>
2014-12-16 22:26:30 +0000

    util-stats: Introduces BlacklistStatsReceivers to ignore stats
    Problem
    
    Having too many stats entails GC pressure.
    
    Solution
    
    Add a BlacklistStatsReceiver to ignore stats.
    
    RB_ID=542888

---
+++ util-stats/src/main/scala/com/twitter/finagle/stats/BlacklistStatsReceiver.scala
@@ -0,0 +1,27 @@
+package com.twitter.finagle.stats
+
+/**
+ * A blacklisting [[StatsReceiver]].  If the name for a metric is found to be
+ * blacklisted, nothing is recorded.
+ *
+ * @param underlying: a base [[StatsReceiver]], used for metrics that aren't
+ *        blacklisted
+ * @param blacklisted: a predicate that reads a name and returns true to
+ *        blacklist, and false to let it pass through
+ */
+class BlacklistStatsReceiver(
+  underlying: StatsReceiver,
+  blacklisted: Seq[String] => Boolean)
+    extends StatsReceiver {
+  val repr = underlying.repr
+  override def isNull = underlying.isNull
+
+  def counter(name: String*): Counter = getStatsReceiver(name).counter(name: _*)
+
+  def stat(name: String*): Stat = getStatsReceiver(name).stat(name: _*)
+
+  def addGauge(name: String*)(f: => Float): Gauge = getStatsReceiver(name).addGauge(name: _*)(f)
+
+  private[this] def getStatsReceiver(name: Seq[String]): StatsReceiver =
+    if (blacklisted(name)) NullStatsReceiver else underlying
+}
---
+++ util-stats/src/test/scala/com/twitter/finagle/stats/BlacklistStatsReceiverTest.scala
@@ -0,0 +1,65 @@
+package com.twitter.finagle.stats
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BlacklistStatsReceiverTest extends FunSuite {
+  test("BlacklistStatsReceiver blacklists properly") {
+    val inmemory = new InMemoryStatsReceiver()
+    val bsr = new BlacklistStatsReceiver(inmemory, { case _ => true })
+    val ctr = bsr.counter("foo", "bar")
+    ctr.incr()
+    val gauge = bsr.addGauge("foo", "baz") { 3.0f }
+    val stat = bsr.stat("qux")
+    stat.add(3)
+
+    assert(inmemory.counters.isEmpty)
+    assert(inmemory.gauges.isEmpty)
+    assert(inmemory.stats.isEmpty)
+  }
+
+  test("BlacklistStatsReceiver allows through properly") {
+    val inmemory = new InMemoryStatsReceiver()
+    val bsr = new BlacklistStatsReceiver(inmemory, { case _ => false })
+    val ctr = bsr.counter("foo", "bar")
+    ctr.incr()
+    val gauge = bsr.addGauge("foo", "baz") { 3.0f }
+    val stat = bsr.stat("qux")
+    stat.add(3)
+
+    assert(inmemory.counters === Map(Seq("foo", "bar") -> 1))
+    assert(inmemory.gauges(Seq("foo", "baz"))() === 3.0)
+    assert(inmemory.stats === Map(Seq("qux") -> Seq(3.0)))
+  }
+
+  test("BlacklistStatsReceiver can go both ways properly") {
+    val inmemory = new InMemoryStatsReceiver()
+    val bsr = new BlacklistStatsReceiver(inmemory, { case seq => seq.length != 2 })
+    val ctr = bsr.counter("foo", "bar")
+    ctr.incr()
+    val gauge = bsr.addGauge("foo", "baz") { 3.0f }
+    val stat = bsr.stat("qux")
+    stat.add(3)
+
+    assert(inmemory.counters === Map(Seq("foo", "bar") -> 1))
+    assert(inmemory.gauges(Seq("foo", "baz"))() === 3.0)
+    assert(inmemory.stats.isEmpty)
+  }
+
+  test("BlacklistStatsReceiver works scoped") {
+    val inmemory = new InMemoryStatsReceiver()
+    val bsr = new BlacklistStatsReceiver(inmemory, { case seq => seq == Seq("foo", "bar") }).scope("foo")
+    val ctr = bsr.counter("foo", "bar")
+    ctr.incr()
+    val gauge = bsr.addGauge("foo", "baz") { 3.0f }
+    val stat = bsr.stat("bar")
+    stat.add(3)
+
+    assert(inmemory.counters === Map(Seq("foo", "foo", "bar") -> 1))
+    assert(inmemory.gauges(Seq("foo", "foo", "baz"))() === 3.0)
+    assert(inmemory.stats.isEmpty)
+  }
+
+}
