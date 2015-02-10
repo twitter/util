44d71050255dd35dcab7d6035a00b3bfd8ca2c64
Joshua Samberg <j.samberg@samsung.com>
2015-02-10 10:15:00 -0500

    Do the scaladoc the Effective Scala way!

--- util-core/src/main/scala/com/twitter/util/Batcher.scala
+++ util-core/src/main/scala/com/twitter/util/Batcher.scala
@@ -6,10 +6,11 @@ class Batcher[In, Out] private[util](
 )(
   implicit timer: Timer
 ) extends Function1[In, Future[Out]] { batcher =>
-  /** Enqueues requests for a batched [[com.twitter.util.Future]]
-    *
-    * Processes all enqueued requests after the batch size is reached
-    */
+  /**
+   * Enqueues requests for a batched [[com.twitter.util.Future]]
+   *
+   * Processes all enqueued requests after the batch size is reached
+   */
   def apply(t: In): Future[Out] = executor.enqueue(t)
 
   /** Immediately processes all unprocessed requests */
