e80145e3725981c9ea8ef8f2281e5d48c78b7e62
Joshua Samberg <j.samberg@samsung.com>
2015-02-10 03:25:39 -0500

    Make Batcher constructor private[util], not Batcher class

--- util-core/src/main/scala/com/twitter/util/Batcher.scala
+++ util-core/src/main/scala/com/twitter/util/Batcher.scala
@@ -1,12 +1,15 @@
 package com.twitter.util
 
 /** Provides a clean, lightweight interface for working with batched Futures */
-private[util] class Batcher[In, Out](
+class Batcher[In, Out] private[util](
   executor: BatchExecutor[In, Out]
 )(
   implicit timer: Timer
 ) extends Function1[In, Future[Out]] { batcher =>
-  /** Enqueues requests for a batched Future */
+  /** Enqueues requests for a batched Future
+    *
+    * When the batch size is reached, all enqueued requests are processed
+    */
   def apply(t: In): Future[Out] = executor.enqueue(t)
 
   /** Immediately processes all unprocessed requests */
