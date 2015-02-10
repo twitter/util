28d77ac6331949e68a868e01bbc90bc866663058
Joshua Samberg <j.samberg@samsung.com>
2015-02-10 08:52:35 -0500

    Refined scaladoc for Batcher

--- util-core/src/main/scala/com/twitter/util/Batcher.scala
+++ util-core/src/main/scala/com/twitter/util/Batcher.scala
@@ -1,14 +1,14 @@
 package com.twitter.util
 
-/** Provides a clean, lightweight interface for working with batched Futures */
+/** Provides a clean, lightweight interface for working with a batched [[com.twitter.util.Future]] */
 class Batcher[In, Out] private[util](
   executor: BatchExecutor[In, Out]
 )(
   implicit timer: Timer
 ) extends Function1[In, Future[Out]] { batcher =>
-  /** Enqueues requests for a batched Future
+  /** Enqueues requests for a batched [[com.twitter.util.Future]]
     *
-    * When the batch size is reached, all enqueued requests are processed
+    * Processes all enqueued requests after the batch size is reached
     */
   def apply(t: In): Future[Out] = executor.enqueue(t)
 
