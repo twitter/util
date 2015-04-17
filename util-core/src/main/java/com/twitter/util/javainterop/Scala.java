3ceafd4f783795f7da0af3e05f5116312d6c7ebf
Kevin Oliver <koliver@twitter.com>
2015-04-17 21:41:19 +0000

    util-core: Add Java to immutable Scala collection conversions
    Problem
    
    It is difficult to convert from a `java.util.Map` to a
    `scala.collection.immutable.Map` from Java due to the implicit
    evidence needed by `toMap`.
    
    Solution
    
    Introduce `c.t.u.javainterop.Scala` methods to help Java devs convert
    into immutable Scala collections. While the other variants are not
    strictly necessary from Java, the uniform API should make it easier
    for devs.
    
    RB_ID=634464

---
+++ util-core/src/main/java/com/twitter/util/javainterop/Scala.java
@@ -0,0 +1,76 @@
+package com.twitter.util.javainterop;
+
+import scala.Tuple2;
+
+import static scala.collection.JavaConversions.asScalaBuffer;
+import static scala.collection.JavaConversions.asScalaSet;
+
+/**
+ * A collection of conversions from Java collections to their immutable
+ * Scala variants.
+ *
+ * See scala.collection.JavaConversions if you do not need immutable
+ * collections.
+ */
+public final class Scala {
+
+  private Scala() { throw new IllegalStateException(); }
+
+  /**
+   * Converts a {@link java.util.List} to an immutable Scala Seq.
+   *
+   * See scala.collection.JavaConversions.asScalaBuffer if you do
+   * not need the returned Seq to be immutable.
+   *
+   * @return an empty Seq if the input is null.
+   */
+  @SuppressWarnings("unchecked")
+  public static <E> scala.collection.immutable.Seq<E> asImmutableSeq(
+      java.util.List<E> jList
+  ) {
+    if (jList == null) {
+      return scala.collection.immutable.Seq$.MODULE$.<E>empty();
+    } else {
+      return asScalaBuffer(jList).toList();
+    }
+  }
+
+  /**
+   * Converts a {@link java.util.Set} to an immutable Scala Set.
+   *
+   * See scala.collection.JavaConversions.asScalaSet if you do
+   * not need the returned Set to be immutable.
+   *
+   * @return an empty Set if the input is null.
+   */
+  public static <E> scala.collection.immutable.Set<E> asImmutableSet(
+      java.util.Set<E> jSet
+  ) {
+    if (jSet == null) {
+      return scala.collection.immutable.Set$.MODULE$.empty();
+    } else {
+      return asScalaSet(jSet).toSet();
+    }
+  }
+
+  /**
+   * Converts a {@link java.util.Map} to an immutable Scala Map.
+   *
+   * See scala.collection.JavaConversions.asScalaMap if you do
+   * not need the returned Map to be immutable.
+   *
+   * @return an empty Map if the input is null.
+   */
+  public static <K, V> scala.collection.immutable.Map<K, V> asImmutableMap(
+      java.util.Map<K, V> jMap
+  ) {
+    scala.collection.immutable.Map<K, V> sMap = scala.collection.immutable.Map$.MODULE$.empty();
+    if (jMap != null) {
+      for (java.util.Map.Entry<K, V> entry : jMap.entrySet()) {
+        sMap = sMap.$plus(new Tuple2<K, V>(entry.getKey(), entry.getValue()));
+      }
+    }
+    return sMap;
+  }
+
+}
---
+++ util-core/src/test/java/com/twitter/util/javainterop/ScalaTest.java
@@ -0,0 +1,79 @@
+package com.twitter.util.javainterop;
+
+import java.util.*;
+
+import org.junit.Test;
+
+import static com.twitter.util.javainterop.Scala.*;
+
+import static org.junit.Assert.*;
+
+public class ScalaTest {
+
+  @Test
+  public void testAsImmutableSeq() {
+    assertEquals(0, asImmutableSeq(null).size());
+
+    List<String> list = new ArrayList<>();
+    assertEquals(0, asImmutableSeq(list).size());
+
+    list.add("1");
+    scala.collection.immutable.Seq<String> sList1 = asImmutableSeq(list);
+    assertEquals(1, sList1.size());
+    assertEquals("1", list.get(0));
+
+    list.add("2");
+    // make sure that didn't change the 1st list (in other words, make sure they
+    // aren't sharing the underlying data structure).
+    assertEquals(1, sList1.size());
+    scala.collection.immutable.Seq<String> sList2 = asImmutableSeq(list);
+    assertEquals(2, sList2.size());
+    assertEquals("1", sList2.apply(0));
+    assertEquals("2", sList2.apply(1));
+  }
+
+  @Test
+  public void testAsImmutableSet() {
+    assertEquals(0, asImmutableSet(null).size());
+
+    Set<String> set = new HashSet<>();
+    assertEquals(0, asImmutableSet(set).size());
+
+    set.add("1");
+    scala.collection.immutable.Set<String> sSet1 = asImmutableSet(set);
+    assertEquals(1, sSet1.size());
+    assertTrue(sSet1.contains("1"));
+
+    set.add("2");
+    // make sure that didn't change the 1st list (in other words, make sure they
+    // aren't sharing the underlying data structure).
+    assertEquals(1, sSet1.size());
+    scala.collection.immutable.Set<String> sSet2 = asImmutableSet(set);
+    assertEquals(2, sSet2.size());
+    assertTrue(sSet2.contains("1"));
+    assertTrue(sSet2.contains("2"));
+  }
+
+  @Test
+  public void testAsImmutableMap() {
+    assertEquals(0, asImmutableMap(null).size());
+
+    Map<String, String> map = new HashMap<>();
+    assertEquals(0, asImmutableMap(map).size());
+
+    map.put("1", "a");
+    scala.collection.immutable.Map<String, String> sMap1 = asImmutableMap(map);
+    assertEquals(1, sMap1.size());
+    assertEquals("a", sMap1.apply("1"));
+
+    map.put("2", "b");
+    // make sure that didn't change the 1st map (in other words, make sure they
+    // aren't sharing the underlying data structure).
+    assertEquals(1, sMap1.size());
+    scala.collection.immutable.Map<String, String> sMap2 = asImmutableMap(map);
+    assertEquals(2, sMap2.size());
+    assertEquals("a", sMap2.apply("1"));
+    assertEquals("b", sMap2.apply("2"));
+  }
+
+}
