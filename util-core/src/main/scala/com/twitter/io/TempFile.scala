a86c170f9c932fadf006e880a4cc216a8437188d
Peter Seibel <pseibel@twitter.com>
2015-04-29 21:02:18 +0000

    core-util: Adding fromSystemResourcePath method for use in classpath isolation test fixing.
    Problem
    
    TempFile only provides methods for getting resources relative to specific classes, not via the system classloader.
    
    Solution
    
    Implement a new method, fromSystemResourcePath.
    
    Result
    
    The method will exist.
    
    RB_ID=647588

--- util-core/src/main/scala/com/twitter/io/TempFile.scala
+++ util-core/src/main/scala/com/twitter/io/TempFile.scala
@@ -1,6 +1,6 @@
 package com.twitter.io
 
-import java.io.{BufferedOutputStream, File, FileNotFoundException, FileOutputStream}
+import java.io._
 
 object TempFile {
   /**
@@ -30,21 +30,32 @@ object TempFile {
    * @param path the resource-relative path to make a temp file from
    * @return the temp File object
    */
-  def fromResourcePath(klass: Class[_], path: String): File = {
-    val (basename, ext) = {
-      val last = path.split(File.separatorChar).last
-      last.split('.').reverse match {
-        case Array(basename) =>
-          (basename, "")
-        case Array(ext, base@_*) =>
-          (base.reverse.mkString("."), ext)
-      }
-    }
+  def fromResourcePath(klass: Class[_], path: String): File =
+    fromStream(path, klass.getResourceAsStream(path))
 
-    klass.getResourceAsStream(path) match {
+  /**
+   * Create a temporary file from the given system resource path
+   * (i.e. via ClassLoader.getSystemResource). The tempfile is deleted
+   * on JVM exit.
+   *
+   * Note, due to the usage of `File.deleteOnExit()` callers should
+   * be careful using this as it can leak memory.
+   * See http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4513817 for
+   * example.
+   *
+   * @param path the path to make a temp file from
+   * @return the temp File object
+   */
+  def fromSystemResourcePath(path: String): File =
+    fromStream(path, ClassLoader.getSystemResourceAsStream(path))
+
+
+  private[this] def fromStream(path: String, stream: InputStream): File =
+    stream match {
       case null =>
         throw new FileNotFoundException(path)
       case stream =>
+        val (basename, ext) = parsePath(path)
         val file = File.createTempFile(basename, "." + ext)
         file.deleteOnExit()
         val fos = new BufferedOutputStream(new FileOutputStream(file), 1<<20)
@@ -54,5 +65,11 @@ object TempFile {
         stream.close()
         file
     }
-  }
+
+  private[this] def parsePath(path: String) =
+    path.split(File.separatorChar).last.split('.').reverse match {
+      case Array(basename) => (basename, "")
+      case Array(ext, base@_*) => (base.reverse.mkString("."), ext)
+    }
+
 }
---
+++ util-core/src/test/scala/com/twitter/io/TempFileTest.scala
@@ -0,0 +1,31 @@
+package com.twitter.io
+
+import java.io.{ByteArrayInputStream, DataInputStream, File}
+import java.util.Arrays
+
+import org.junit.runner.RunWith
+import org.scalatest.WordSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TempFileTest extends WordSpec {
+
+  "TempFile" should {
+
+    "load resources" in {
+      val f1 = TempFile.fromResourcePath("/java/lang/String.class")
+      val f2 = TempFile.fromResourcePath(getClass, "/java/lang/String.class")
+      val f3 = TempFile.fromSystemResourcePath("java/lang/String.class")
+
+      val c1 = Files.readBytes(f1)
+      val c2 = Files.readBytes(f2)
+      val c3 = Files.readBytes(f3)
+
+      assert(Arrays.equals(c1, c2))
+      assert(Arrays.equals(c2, c3))
+      assert(new DataInputStream(new ByteArrayInputStream(c1)).readInt == 0xcafebabe)
+    }
+
+  }
+
+}
