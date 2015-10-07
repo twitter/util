0a79a10db031af12f971800fdc5b944944f76ba3
Moses Nakamura <mnakamura@twitter.com>
2015-10-07 09:15:12 +0000

    finagle-core: Stop recording transit latency and deadline budget for clients
    Problem
    
    Finagle records transit latency for clients, but only servers
    care about it.
    
    Solution
    
    Move the transit latency stat out of StatsFilter and into
    ServerStatsFilter. Handletime is also a server-specific stat,
    so moved that into ServerStatsFilter too, and deleted
    HandletimeFilter.  This has the added advantage of recording
    transit latency at the same time we do handletime, which is one
    of the earliest points.
    
    This review also handles some other miscellaneous cleanup,
    making no-allocation, testable, elapsed duration easier to use,
    adding tests for handletime, transit latency and deadline budget.
    
    Result
    
    Finagle services no longer export transit_latency_ms or
    deadline_budget_ms for clients.  It's not useful for clients,
    so it's safe to remove it.
    
    RB_ID=751268

--- util-core/src/main/scala/com/twitter/util/Stopwatch.scala
+++ util-core/src/main/scala/com/twitter/util/Stopwatch.scala
@@ -1,5 +1,7 @@
 package com.twitter.util
 
+import java.util.concurrent.TimeUnit
+
 /**
  * A stopwatch may be used to measure elapsed time.
  */
@@ -21,15 +23,73 @@ trait Stopwatch {
  */
 object Stopwatch extends Stopwatch {
 
-  private[this] val systemNanoFn = () => System.nanoTime()
+  /**
+   * A function that returns a Long that can be used for measuring elapsed time
+   * in nanoseconds, using the system clock.
+   *
+   * Note that invoking this doesn't entail any allocations.
+   */
+  val systemNanos: () => Long =
+    // we use nanos instead of current time millis because it increases monotonically
+    () => System.nanoTime()
+
+  /**
+   * A function that returns a Long that can be used for measuring elapsed time
+   * in nanoseconds, that is `Time` manipulation compatible.
+   *
+   * Useful for testing, but should not be used in production, since it uses a
+   * non-monotonic time under the hood, and entails a few allocations.  For
+   * production, see `systemNanos`.
+   */
+  val timeNanos: () => Long = () => Time.now.inNanoseconds
+
+  /**
+   * A function that returns a Long that can be used for measuring elapsed time
+   * in microseconds, using the system clock.
+   *
+   * Note that invoking this doesn't entail any allocations.
+   */
+  val systemMicros: () => Long =
+    // we use nanos instead of current time millis because it increases monotonically
+    () => TimeUnit.MICROSECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS)
+
+  /**
+   * A function that returns a Long that can be used for measuring elapsed time
+   * in microseconds, that is `Time` manipulation compatible.
+   *
+   * Useful for testing, but should not be used in production, since it uses a
+   * non-monotonic time under the hood, and entails a few allocations.  For
+   * production, see `systemMicros`.
+   */
+  val timeMicros: () => Long = () => Time.now.inMicroseconds
+
+  /**
+   * A function that returns a Long that can be used for measuring elapsed time
+   * in milliseconds, using the system clock.
+   *
+   * Note that invoking this doesn't entail any allocations.
+   */
+  val systemMillis: () => Long =
+    // we use nanos instead of current time millis because it increases monotonically
+    () => TimeUnit.MILLISECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS)
+
+  /**
+   * A function that returns a Long that can be used for measuring elapsed time
+   * in milliseconds, that is `Time` manipulation compatible.
+   *
+   * Useful for testing, but should not be used in production, since it uses a
+   * non-monotonic time under the hood, and entails a few allocations.  For
+   * production, see `systemMillis`.
+   */
+  val timeMillis: () => Long = () => Time.now.inMilliseconds
 
   def start(): Elapsed = Time.localGetTime() match {
     case Some(local) =>
       val startAt: Time = local()
       () => local() - startAt
     case None =>
-      val startAt: Long = systemNanoFn()
-      () => Duration.fromNanoseconds(systemNanoFn() - startAt)
+      val startAt: Long = systemNanos()
+      () => Duration.fromNanoseconds(systemNanos() - startAt)
   }
 
   /**
