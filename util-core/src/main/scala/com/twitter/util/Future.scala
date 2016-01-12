fca034d2f5b2e9778e22e042707d0610298a044a
Chandler Abraham <cabraham@twitter.com>
2016-01-12 02:22:00 +0000

    util-core: traverseSequentially
    RB_ID=785091

--- util-core/src/main/scala/com/twitter/util/Future.scala
+++ util-core/src/main/scala/com/twitter/util/Future.scala
@@ -343,6 +343,33 @@ def join[%s](%s): Future[(%s)] = join(Seq(%s)) map { _ => (%s) }""".format(
   def join[A](fs: JList[Future[A]]): Future[Unit] = Futures.join(fs)
 
   /**
+   * Take a sequence and sequentially apply a function `f` to each item.
+   * Then return all future results `as` a single Future[Seq[_]].
+   *
+   * If during execution any `f` is satsified `as` a failure ([[Future.exception]])
+   * then that failed Future will be returned and the remaining elements of `as`
+   * will not be processed.
+   *
+   * usage:
+   *  {{{
+   *    // will return a Future of `Seq(2, 3, 4)`
+   *    Future.traverseSequentially(Seq(1, 2, 3)) { i =>
+   *      Future(i + 1)
+   *    }
+   *  }}}
+   *
+   * @param `as` a sequence of `A` that will have `f` applied to each item sequentially
+   * @return a `Future[Seq[B]]` containing the results of `f` being applied to every item in `as`
+   */
+  def traverseSequentially[A,B](as: Seq[A])(f: A => Future[B]): Future[Seq[B]] =
+    as.foldLeft(Future.value(Vector.empty[B])) { (resultsFuture, nextItem) =>
+      for {
+        results    <- resultsFuture
+        nextResult <- f(nextItem)
+      } yield (results :+ nextResult)
+    }
+
+  /**
    * Collect the results from the given futures into a new future of
    * Seq[A]. If one or more of the given Futures is exceptional, the resulting
    * Future result will be the first exception encountered.
