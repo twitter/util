59ddeb1aa921c59857557b1c5ad30592d1721d06
Christopher Coco <ccoco@twitter.com>
2016-03-10 23:14:38 +0000

    Bump versions - util/ostrich/finagle/scrooge/twitter-server
    Prepare libraries for release.
    
    RB_ID=809587

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Util extends Build {
   val branch = Process("git" :: "rev-parse" :: "--abbrev-ref" :: "HEAD" :: Nil).!!.trim
   val suffix = if (branch == "master") "" else "-SNAPSHOT"
 
-  val libVersion = "6.32.0" + suffix
+  val libVersion = "6.33.0" + suffix
   val zkVersion = "3.5.0-alpha"
   val zkClientVersion = "0.0.79"
   val zkGroupVersion = "0.0.90"
