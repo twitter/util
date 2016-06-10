98cedf187c1af9dd1e11177482c601c817343292
Matthew Bilotti <mbilotti@twitter.com>
2016-06-10 01:56:46 +0000

    source: upgrade jackson to 2.6.5
    RB_ID=836819

--- project/Build.scala
+++ project/Build.scala
@@ -342,8 +342,8 @@ object Util extends Build {
     libraryDependencies ++= Seq(
       "thrift"                     % "libthrift"        % "0.5.0",
       "org.slf4j"                  % "slf4j-api"        % "1.7.7" % "provided",
-      "com.fasterxml.jackson.core" % "jackson-core"     % "2.4.4",
-      "com.fasterxml.jackson.core" % "jackson-databind" % "2.4.4"
+      "com.fasterxml.jackson.core" % "jackson-core"     % "2.6.5",
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.5"
     )
   ).dependsOn(utilCodec)
 
