1817b56a8610d9d21b7139de52cb89eaf6cf82a5
Moses Nakamura <mnakamura@twitter.com>
2016-06-15 00:03:50 +0000

    util-core: Clarify Once behavior when we throw an exception
    RB_ID=842665
    TBR=true

--- util-core/src/main/scala/com/twitter/concurrent/Once.scala
+++ util-core/src/main/scala/com/twitter/concurrent/Once.scala
@@ -6,6 +6,8 @@ object Once {
    * again.  If the returned function is invoked while another thread is running
    * `fn` for the first (and only) time, the interloping thread blocks until
    * `fn` has finished.
+   *
+   * If `fn` throws, it may be run more than once.
    */
   def apply(fn: => Unit): () => Unit = {
     new Function0[Unit] {
