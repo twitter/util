4cd71b810432fb4fae351c8e14eca2112caea4cd
Vladimir Kostyukov <vkostyukov@twitter.com>
2016-07-07 22:41:00 +0000

    csl: Release CSL libraries
    Problem / Solution
    
    Finagle 6.36
    Util 6.35
    Ostrich 9.19
    TwitterServer 1.21
    Finatra 2.2.0
    Scrooge 4.8.0
    
    RB_ID=849873

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Util extends Build {
   val branch = Process("git" :: "rev-parse" :: "--abbrev-ref" :: "HEAD" :: Nil).!!.trim
   val suffix = if (branch == "master") "" else "-SNAPSHOT"
 
-  val libVersion = "6.34.0" + suffix
+  val libVersion = "6.35.0" + suffix
   val zkVersion = "3.5.0-alpha"
   val zkClientVersion = "0.0.79"
   val zkGroupVersion = "0.0.90"
