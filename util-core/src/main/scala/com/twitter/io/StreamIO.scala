4775b0730f1812220d6583c1be2d77086ca76018
Sang Dang <zkidkid@gmail.com>
2016-09-24 10:38:18 +0700

    reformat to twitter style

--- util-core/src/main/scala/com/twitter/io/StreamIO.scala
+++ util-core/src/main/scala/com/twitter/io/StreamIO.scala
@@ -6,39 +6,39 @@ import scala.annotation.tailrec
 
 object StreamIO {
   /**
-    * Copy an InputStream to an OutputStream in chunks of the given
-    * buffer size (default = 1KB).
-    */
+   * Copy an InputStream to an OutputStream in chunks of the given
+   * buffer size (default = 1KB).
+   */
   final def copy(
-                  inputStream: InputStream,
-                  outputStream: OutputStream,
-                  bufferSize: Int = 1024
-                ) {
+    inputStream: InputStream,
+    outputStream: OutputStream,
+    bufferSize: Int = 1024
+  ) {
     val buffer = new Array[Byte](bufferSize)
     copy(inputStream, outputStream, buffer)
   }
 
+  /**
+   * Buffer (fully) the given input stream by creating & copying it to
+   * a ByteArrayOutputStream.
+   */
+  def buffer(inputStream: InputStream): ByteArrayOutputStream = {
+    val bos = new java.io.ByteArrayOutputStream
+    copy(inputStream, bos)
+    bos
+  }
+
   @tailrec
   final private def copy(
-                  inputStream: InputStream,
-                  outputStream: OutputStream,
-                  buffer: Array[Byte]
-                ) {
+    inputStream: InputStream,
+    outputStream: OutputStream,
+    buffer: Array[Byte]
+  ) {
     inputStream.read(buffer, 0, buffer.length) match {
       case -1 => ()
       case n =>
         outputStream.write(buffer, 0, n)
         copy(inputStream, outputStream, buffer)
     }
-  }
-
-  /**
-    * Buffer (fully) the given input stream by creating & copying it to
-    * a ByteArrayOutputStream.
-    */
-  def buffer(inputStream: InputStream): ByteArrayOutputStream = {
-    val bos = new java.io.ByteArrayOutputStream
-    copy(inputStream, bos)
-    bos
   }
 }
