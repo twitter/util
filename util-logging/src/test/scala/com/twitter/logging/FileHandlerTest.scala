b8fc0998bcffec2329bee34b3ad7196f55ce6fbb
Vivek Malik <vivek.malik@lookout.com>
2016-10-05 16:56:31 -0400

    util-logging: Pick up log file size
    Problem
    
    FileHandler sets 'bytesWrittenToFile' as 0 in 'openLog()'.
    This forces log file size to 0 every-time application is restarted, even if it exists.
    
    Solution
    
    Set 'bytesWrittenToFile' as file size in openStream(), if log file exists.
    Else set it to 0. Remove 'bytesWrittenToFile' from 'openLog()'
    
    ISSUE: #176

--- util-logging/src/test/scala/com/twitter/logging/FileHandlerTest.scala
+++ util-logging/src/test/scala/com/twitter/logging/FileHandlerTest.scala
@@ -302,5 +302,49 @@ class FileHandlerTest extends WordSpec with TempFolder {
         handler.close()
       }
     }
+
+    /**
+      * Execute this test individually to affirm that the roll policy based upon the size works
+      * even after the test case is executed couple of times.
+      * This test mimics the scenario that, if log file exists, FileHandler should pick up it's size
+      * and roll over at the set limit, in order to prevent the file size from growing.
+      * The test succeeds if the roll over takes place as desired else fails.
+      */
+    withTempFolder {
+      1 to 5 foreach{testRun =>
+        s" should succeed to rollover at set limit when test is restarted, execution ${testRun}" in {
+          val logLevel = Level.INFO
+          val fileSizeInMegaBytes: Long = 1
+          val record1 = new javalog.LogRecord(logLevel, "Sending bytes to fill up file")
+          val handler = FileHandler(
+            filename = folderName+"/LogFileDir/testFileSize.log",
+            rollPolicy = Policy.MaxSize(fileSizeInMegaBytes.megabytes),
+            rotateCount = 8,
+            append = true,
+            formatter = BareFormatter
+          ).apply()
+
+          for (a <- 1 to 10000){
+            handler.publish(record1)
+          }
+
+          def listLogFiles(dir: String):List[File] = {
+            val d = new File(dir)
+            if (d.exists && d.isDirectory) {
+              d.listFiles.filter(_.isFile).toList
+            } else {
+              List[File]()
+            }
+          }
+          val files = listLogFiles(folderName+"/LogFileDir")
+          files.foreach{f: File =>
+            val len = f.length().bytes
+            if (len > fileSizeInMegaBytes.megabytes){
+              fail("Failed to roll over the log file")
+            }
+          }
+        }
+      }
+    }
   }
 }
