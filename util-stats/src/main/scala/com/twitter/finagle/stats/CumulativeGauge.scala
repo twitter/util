c5a72d101f1abe3c3930310bdbec01a0db7a39ea
Moses Nakamura <mnakamura@twitter.com>
2016-12-02 20:58:06 +0000

    util-stats: Fixes a race condition in CumulativeGauge
    Problem
    
    CumulativeGauge had a problem where we could lose gauges when they flapped
    between 0 gauges for a name and 1 gauge for a name.
    
    Solution
    
    Java 8 introduces ConcurrentHashMap#computeIfAbsent which simplifies some of the
    updating code.  It also introduces StampedLock, which we use as a non-reentrant ReadWriteLock to tear down the
    gauge permanently after every gauge has declared it wants to be torn down.  Once
    we've torn down the gauge, we return a null, signalling to the caller that it
    must make a new CumulativeGauge.
    
    At the same time, we tighten the lint which says you have too many cumulative gauges.
    
    Result
    
    CumulativeGauge is now threadsafe.
    
    RB_ID=892236

--- util-stats/src/main/scala/com/twitter/finagle/stats/CumulativeGauge.scala
+++ util-stats/src/main/scala/com/twitter/finagle/stats/CumulativeGauge.scala
@@ -2,8 +2,11 @@ package com.twitter.finagle.stats
 
 import com.github.benmanes.caffeine.cache.{Cache, Caffeine, RemovalCause, RemovalListener}
 import com.twitter.util.lint._
-import java.lang.Boolean
+import java.lang.{Boolean => JBoolean}
 import java.util.concurrent.{ConcurrentHashMap, Executor, ForkJoinPool}
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.locks.StampedLock
+import java.util.function.{Function => JFunction}
 import scala.collection.JavaConverters._
 
 /**
@@ -23,8 +26,8 @@ private[finagle] abstract class CumulativeGauge(executor: Executor) { self =>
     def remove(): Unit = self.remove(this)
   }
 
-  private[this] val removals = new RemovalListener[UnderlyingGauge, java.lang.Boolean] {
-    def onRemoval(key: UnderlyingGauge, value: Boolean, cause: RemovalCause): Unit =
+  private[this] val removals = new RemovalListener[UnderlyingGauge, JBoolean] {
+    def onRemoval(key: UnderlyingGauge, value: JBoolean, cause: RemovalCause): Unit =
       self.deregister()
   }
 
@@ -35,12 +38,12 @@ private[finagle] abstract class CumulativeGauge(executor: Executor) { self =>
    *
    * @see [[cleanup()]] to allow for more predictable testing.
    */
-  private[this] val refs: Cache[UnderlyingGauge, java.lang.Boolean] =
+  private[this] val refs: Cache[UnderlyingGauge, JBoolean] =
     Caffeine.newBuilder()
       .executor(executor)
       .weakKeys()
       .removalListener(removals)
-      .build[UnderlyingGauge, java.lang.Boolean]()
+      .build[UnderlyingGauge, JBoolean]()
 
   /** Removes expired gauges. Exposed for testing. */
   protected def cleanup(): Unit =
@@ -59,12 +62,14 @@ private[finagle] abstract class CumulativeGauge(executor: Executor) { self =>
   private def remove(underlyingGauge: UnderlyingGauge): Unit =
     refs.invalidate(underlyingGauge)
 
+  /**
+   * Returns a gauge unless it is dead, in which case it returns null.
+   */
   def addGauge(f: => Float): Gauge = {
     val underlyingGauge = new UnderlyingGauge(() => f)
-    refs.put(underlyingGauge, java.lang.Boolean.TRUE)
-    register()
+    refs.put(underlyingGauge, JBoolean.TRUE)
 
-    underlyingGauge
+    if (register()) underlyingGauge else null
   }
 
   def getValue: Float = {
@@ -77,14 +82,18 @@ private[finagle] abstract class CumulativeGauge(executor: Executor) { self =>
     sum
   }
 
+  // Special care must be taken in implementing these so that they are free
+  // of race conditions.
   /**
-   * These need to be implemented by the gauge provider. They indicate
-   * when the gauge needs to be registered & deregistered.
+   * Indicates when the gauge needs to be registered.
    *
-   * Special care must be taken in implementing these so that they are free
-   * of race conditions.
+   * Returns true until the gauge is dead, after which it returns false.
    */
-  def register(): Unit
+  def register(): Boolean
+
+  /**
+   * Indicates when the gauge needs to be deregistered.
+   */
   def deregister(): Unit
 }
 
@@ -96,12 +105,12 @@ trait StatsReceiverWithCumulativeGauges extends StatsReceiver { self =>
     Rule(
       Category.Performance,
       "Large CumulativeGauges",
-      "Identifies CumulativeGauges which are backed by very large numbers (100k+) " +
+      "Identifies CumulativeGauges which are backed by very large numbers (10k+) " +
         "of Gauges. Indicative of a leak or code registering the same gauge more " +
         s"often than expected. (For $toString)"
     ) {
       val largeCgs = gauges.asScala.flatMap { case (ks, cg) =>
-        if (cg.totalSize >= 100000) Some(ks -> cg.totalSize)
+        if (cg.totalSize >= 10000) Some(ks -> cg.totalSize)
         else None
       }
       if (largeCgs.isEmpty) {
@@ -121,35 +130,65 @@ trait StatsReceiverWithCumulativeGauges extends StatsReceiver { self =>
   protected[this] def registerGauge(name: Seq[String], f: => Float): Unit
   protected[this] def deregisterGauge(name: Seq[String]): Unit
 
-  def addGauge(name: String*)(f: => Float): Gauge = {
-    var cumulativeGauge = gauges.get(name)
-    if (cumulativeGauge == null) {
-      val insert = new CumulativeGauge { cg =>
-        // thread safety provided by synchronization on `this/cg`
-        private[this] var registers = 0
+  private[this] val gaugeFn: JFunction[Seq[String], CumulativeGauge] =
+    new JFunction[Seq[String], CumulativeGauge] {
+      // the gauge's state machine only goes Alive => Dead to simplify trickiness
+      // around garbage collection and races
+      def apply(key: Seq[String]): CumulativeGauge = new CumulativeGauge {
 
-        def register(): Unit = cg.synchronized {
-          registers += 1
-          if (registers == 1) {
-            self.registerGauge(name, getValue)
-            // in order to avoid races with `deregister`, we make sure
-            // we are always in `gauges`.
-            gauges.putIfAbsent(name, this)
+        self.registerGauge(key, getValue)
+
+        // we grab the read lock on increment and decrement, which can race each
+        // other.  we only grab the write lock when we tear down the cumulative
+        // gauge, so that we can ensure no more increments happen after death.
+        private[this] val lock = new StampedLock()
+        private[this] val registers = new AtomicInteger(0)
+
+        // protected by `lock`
+        private[this] var dead = false
+
+        def register(): Boolean = {
+          val stamp = lock.readLock()
+          try {
+            if (dead) return false
+            registers.incrementAndGet()
+          } finally {
+            lock.unlockRead(stamp)
           }
+          true
         }
 
-        def deregister(): Unit = cg.synchronized {
-          registers -= 1
-          if (registers == 0) {
-            gauges.remove(name)
-            self.deregisterGauge(name)
+        def deregister(): Unit = {
+          val readStamp = lock.readLock()
+          if (registers.decrementAndGet() == 0) {
+            // we could skip a cas by using tryConvertToWriteLock, but it makes
+            // the code much uglier.
+            lock.unlockRead(readStamp)
+            val writeStamp = lock.writeLock()
+
+            try {
+              if (!dead && registers.get == 0) {
+                dead = true
+                gauges.remove(key)
+                self.deregisterGauge(key)
+              }
+            } finally {
+              lock.unlockWrite(writeStamp)
+            }
+          } else {
+            lock.unlockRead(readStamp)
           }
         }
       }
-      val prev = gauges.putIfAbsent(name, insert)
-      cumulativeGauge = if (prev == null) insert else prev
     }
-    cumulativeGauge.addGauge(f)
+
+  def addGauge(name: String*)(f: => Float): Gauge = {
+    var gauge: Gauge = null
+    while (gauge == null) {
+      var cumulativeGauge = gauges.computeIfAbsent(name, gaugeFn)
+      gauge = cumulativeGauge.addGauge(f)
+    }
+    gauge
   }
 
   /**
