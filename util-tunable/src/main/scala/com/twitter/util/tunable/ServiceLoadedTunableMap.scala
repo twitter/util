6fb5a19e8e524aa6eadf9e35c40832ae9d21a106
Jillian Crossley <jillianc@twitter.com>
2017-02-28 20:18:18 +0000

    util-tunable: Service-loaded TunableMap
    Problem
    
    We want to be able to load a TunableMap on the class path
    to facilitate subscription to a dynamic file-based configuration
    system.
    
    Solution
    
    Introduce `c.t.util.tunable.ServiceLoadedTunableMap` that loads
    a TunableMap matching an id from the class path.
    
    RB_ID=908868

---
+++ util-tunable/src/main/scala/com/twitter/util/tunable/ServiceLoadedTunableMap.scala
@@ -0,0 +1,40 @@
+package com.twitter.util.tunable
+
+import com.twitter.app.LoadService
+
+/**
+ * [[TunableMap]] loaded through `com.twitter.app.LoadService`.
+ */
+private[twitter] trait ServiceLoadedTunableMap extends TunableMap {
+
+  /**
+   * Unique identifier for this [[TunableMap]]. A service may have multiple
+   * [[TunableMap]]s loaded through `com.twitter.app.LoadService`.
+   */
+  def id: String
+}
+
+private[twitter] object ServiceLoadedTunableMap {
+
+  /**
+   * Uses `com.twitter.app.LoadService` to load the [[TunableMap]] for a given `id`.
+   *
+   * If no matches are found, a [[NullTunableMap]] is returned.
+   *
+   * If more than one match is found, an `IllegalStateException` is thrown.
+   */
+  def apply(id: String): TunableMap = {
+
+    val tunableMaps = LoadService[ServiceLoadedTunableMap]().filter(_.id == id).toList
+
+    tunableMaps match {
+      case Nil =>
+        NullTunableMap
+      case tunableMap :: Nil =>
+        tunableMap
+      case _ =>
+        throw new IllegalStateException(
+          s"Found multiple `ServiceLoadedTunableMap`s for $id: ${tunableMaps.mkString(", ")}")
+    }
+  }
+}
---
+++ util-tunable/src/test/scala/com/twitter/util/tunable/ServiceLoadedTunableMapTest.scala
@@ -0,0 +1,61 @@
+package com.twitter.util.tunable
+
+import org.scalatest.FunSuite
+
+/**
+ * A [[TunableMap]] that forwards all calls to `underlying`.
+ */
+trait TunableMapProxy extends TunableMap {
+
+  protected def underlying: TunableMap
+
+  def apply[T](key: TunableMap.Key[T]): Tunable[T] =
+    underlying(key)
+
+  def size: Int = underlying.size
+
+}
+
+class ServiceLoadedTunableTestClient1 extends ServiceLoadedTunableMap with TunableMapProxy {
+  private val tunableMap = TunableMap.newMutable()
+
+  tunableMap.put("tunableId1", "foo")
+  tunableMap.put("tunableId2", 5)
+
+  protected def underlying: TunableMap = tunableMap
+  def id: String = "client1"
+}
+
+class ServiceLoadedTunableTestClient2 extends ServiceLoadedTunableMap with TunableMapProxy {
+  protected def underlying: TunableMap = NullTunableMap
+  def id: String = "client2"
+}
+
+class ServiceLoadedTunableTestClient2Dup extends ServiceLoadedTunableMap with TunableMapProxy {
+  protected def underlying: TunableMap = NullTunableMap
+  def id: String = "client2"
+}
+
+class ServiceLoadedTunableMapTest extends FunSuite {
+
+  test("NullTunableMap returned when no matches") {
+    val tunableMap = ServiceLoadedTunableMap("Non-existent-id")
+    assert(tunableMap eq NullTunableMap)
+  }
+
+  test("TunableMap returned when there is one match for id") {
+    val tunableMap = ServiceLoadedTunableMap("client1")
+
+    assert(tunableMap.size == 2)
+    assert(tunableMap(TunableMap.Key[String]("tunableId1"))() == Some("foo"))
+    assert(tunableMap(TunableMap.Key[Int]("tunableId2"))() == Some(5))
+  }
+
+  test("IllegalArgumentException thrown when there is more than one ServiceLoadedTunableMap " +
+    "for a given serviceName/id") {
+
+    intercept[IllegalStateException] {
+      ServiceLoadedTunableMap("client2")
+    }
+  }
+}
