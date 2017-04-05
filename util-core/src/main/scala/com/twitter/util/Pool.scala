dc26f82b9370417110c491eb3037135638028a28
qilab <qilab@gmx.com>
2017-04-05 13:17:32 +0900

    add explicit type annotation to public methods
    Problem
    
    Explicit type annotations of publick methods are recomended.
    However, type annotations of some publick methods are missing.
    
    Solution
    
    Write explicit type annotations.

--- util-core/src/main/scala/com/twitter/util/Pool.scala
+++ util-core/src/main/scala/com/twitter/util/Pool.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable
 
 trait Pool[A] {
   def reserve(): Future[A]
-  def release(a: A)
+  def release(a: A): Unit
 }
 
 class SimplePool[A](items: mutable.Queue[Future[A]]) extends Pool[A] {
@@ -16,7 +16,7 @@ class SimplePool[A](items: mutable.Queue[Future[A]]) extends Pool[A] {
 
   private val requests = new mutable.Queue[Promise[A]]
 
-  def reserve() = synchronized {
+  def reserve(): Future[A] = synchronized {
     if (items.isEmpty) {
       val future = new Promise[A]
       requests += future
@@ -26,7 +26,7 @@ class SimplePool[A](items: mutable.Queue[Future[A]]) extends Pool[A] {
     }
   }
 
-  def release(item: A) {
+  def release(item: A): Unit = {
     items += Future[A](item)
     synchronized {
       if (!requests.isEmpty && !items.isEmpty)
@@ -43,9 +43,9 @@ abstract class FactoryPool[A](numItems: Int) extends Pool[A] {
   private val healthyQueue = new HealthyQueue[A](makeItem, numItems, isHealthy)
   private val simplePool = new SimplePool[A](healthyQueue)
 
-  def reserve() = simplePool.reserve()
-  def release(a: A) = simplePool.release(a)
-  def dispose(a: A) {
+  def reserve(): Future[A] = simplePool.reserve()
+  def release(a: A): Unit = simplePool.release(a)
+  def dispose(a: A): Unit = {
     healthyQueue += makeItem()
   }
 
@@ -66,7 +66,7 @@ private class HealthyQueue[A](
     super.+=:(elem)
   }
 
-  override def enqueue(elems: Future[A]*): Unit = synchronized{
+  override def enqueue(elems: Future[A]*): Unit = synchronized {
     super.enqueue(elems: _*)
   }
 
