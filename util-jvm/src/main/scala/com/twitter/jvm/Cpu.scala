9fa550a269d2287b24e94921a352ba954f9f4bfb
Bryce Anderson <banderson@twitter.com>
2017-10-12 16:10:47 +0000

    util-jvm: Add sentinel value for com.twitter.jvm.numProcs
    Summary: Problem / Solution
    
    We want a way to explicitly set the value of the com.twitter.jvm.numProcs
    flag to the value reported by the runtime.
    
    JIRA Issues: CSL-5330
    
    Differential Revision: https://phabricator.twitter.biz/D98561

--- util-jvm/src/main/scala/com/twitter/jvm/Cpu.scala
+++ util-jvm/src/main/scala/com/twitter/jvm/Cpu.scala
@@ -2,8 +2,27 @@ package com.twitter.jvm
 
 import com.twitter.app.GlobalFlag
 
+/**
+ * Flag for the number of available processors
+ *
+ * If unset, the default is obtained from the JVM runtime.
+ * If set to -1.0, the result is forced to the value provided by the JVM runtime.
+ */
 object numProcs
     extends GlobalFlag[Double](
       Runtime.getRuntime.availableProcessors().toDouble,
       "number of logical cores"
-    )
+    ) {
+
+  // We allow -1.0 to be a sentinel value to explicitly signal to
+  // use the value of Runtime.getRuntime.availableProcessors()
+  protected override def getValue: Option[Double] = super.getValue match {
+    case Some(value) if approxEqual(value, -1.0) =>
+      Some(Runtime.getRuntime.availableProcessors().toDouble)
+
+    case other => other
+  }
+
+  private[this] def approxEqual(a: Double, b: Double): Boolean =
+    math.abs(a - b) < 0.01
+}
--- util-jvm/src/test/scala/com/twitter/jvm/NumProcsTest.scala
+++ util-jvm/src/test/scala/com/twitter/jvm/NumProcsTest.scala
@@ -1,25 +1,27 @@
 package com.twitter.jvm
 
-import org.junit.runner.RunWith
-import org.scalatest.FunSpec
-import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
 
-@RunWith(classOf[JUnitRunner])
-class NumProcsTest extends FunSpec {
-  describe("numProcs") {
-    it("should be the number of available processors according to the runtime by default") {
-      assert(System.getProperty("com.twitter.jvm.numProcs") == null)
-      assert(numProcs() == Runtime.getRuntime().availableProcessors())
-    }
+class NumProcsTest extends FunSuite {
 
-    it("should be settable as a flag") {
-      val old = numProcs()
-      numProcs.parse("10.0")
+  test("return the number of available processors according to the runtime by default") {
+    assert(System.getProperty("com.twitter.jvm.numProcs") == null)
+    assert(numProcs() == Runtime.getRuntime().availableProcessors())
+  }
 
-      assert(numProcs() == 10.0)
+  test("settable as a flag") {
+    val old = numProcs()
+    numProcs.parse("10.0")
 
-      numProcs.parse()
-      assert(numProcs() == old)
-    }
+    assert(numProcs() == 10.0)
+
+    numProcs.parse()
+    assert(numProcs() == old)
+  }
+
+  test("return the number of available processors occording to the runtime if set to -1.0") {
+    numProcs.parse("-1.0")
+
+    assert(numProcs() == Runtime.getRuntime.availableProcessors().toDouble)
   }
 }
