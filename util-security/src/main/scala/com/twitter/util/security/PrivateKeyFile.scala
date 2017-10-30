23f4a6a049c55121a4cda34be3b947f3ea4bfc46
Ryan O'Neill <ryano@twitter.com>
2017-10-30 19:05:55 +0000

    util-security: Make Ability to Read PrivateKey Files Public
    Summary: Problem / Solution
    
    Currently, the reading of a PKCS#8 PEM formatted `PrivateKey` file is
    contained within finagle-netty4. This code is useful outside of its
    current context and thus should be made publicly accessible. Move the
    code to `PrivateKeyFile` in util-security.
    
    JIRA Issues: CSL-5427
    
    Differential Revision: https://phabricator.twitter.biz/D105266

---
+++ util-security/src/main/scala/com/twitter/util/security/PrivateKeyFile.scala
@@ -0,0 +1,69 @@
+package com.twitter.util.security
+
+import com.twitter.logging.Logger
+import com.twitter.util.{Throw, Try}
+import com.twitter.util.security.PrivateKeyFile._
+import java.io.File
+import java.security.{KeyFactory, PrivateKey}
+import java.security.spec.{InvalidKeySpecException, PKCS8EncodedKeySpec}
+
+/**
+ * A representation of a PrivateKey that is stored in a file
+ * in Pkcs8 PEM format.
+ *
+ * @example
+ * -----BEGIN PRIVATE KEY-----
+ * base64encodedbytes
+ * -----END PRIVATE KEY-----
+ */
+class PrivateKeyFile(file: File) {
+
+  private[this] def logException(ex: Throwable): Unit =
+    log.warning(s"PrivateKeyFile (${file.getName()}) failed to load: ${ex.getMessage()}.")
+
+  /**
+   * Attempts to convert a `PKCS8EncodedKeySpec` to a `PrivateKey`.
+   *
+   * @note
+   * keeps identical behavior to netty
+   * https://github.com/netty/netty/blob/netty-4.1.11.Final/handler/src/main/java/io/netty/handler/ssl/SslContext.java#L1006
+   */
+  private[this] def keySpecToPrivateKey(keySpec: PKCS8EncodedKeySpec): Try[PrivateKey] =
+    Try {
+      KeyFactory.getInstance("RSA").generatePrivate(keySpec)
+    }.handle {
+        case _: InvalidKeySpecException =>
+          KeyFactory.getInstance("DSA").generatePrivate(keySpec)
+      }
+      .handle {
+        case _: InvalidKeySpecException =>
+          KeyFactory.getInstance("EC").generatePrivate(keySpec)
+      }
+      .rescue {
+        case ex: InvalidKeySpecException =>
+          Throw(new InvalidKeySpecException("None of RSA, DSA, EC worked", ex))
+      }
+
+  /**
+   * Attemps to read the contents of an unencrypted PrivateKey file.
+   *
+   * @throws InvalidPemFormatException if the file is not a PEM format
+   * file.
+   *
+   * @throws InvalidKeySpecException if the file cannot be loaded as an
+   * RSA, DSA, or EC PrivateKey.
+   *
+   * @note This method will attempt to load the key first as an RSA key, then
+   * as a DSA key, and then as an EC (Elliptic Curve) key.
+   */
+  def readPrivateKey(): Try[PrivateKey] =
+    new Pkcs8EncodedKeySpecFile(file)
+      .readPkcs8EncodedKeySpec()
+      .flatMap(keySpecToPrivateKey)
+      .onFailure(logException)
+
+}
+
+private object PrivateKeyFile {
+  private val log = Logger.get("com.twitter.util.security")
+}
---
+++ util-security/src/test/scala/com/twitter/util/security/PrivateKeyFileTest.scala
@@ -0,0 +1,61 @@
+package com.twitter.util.security
+
+import com.twitter.io.TempFile
+import java.security.spec.InvalidKeySpecException
+import org.scalatest.FunSuite
+
+class PrivateKeyFileTest extends FunSuite {
+
+  test("File is garbage") {
+    // Lines were manually deleted from a real pkcs 8 pem file
+    val tempFile = TempFile.fromResourcePath("/keys/test-pkcs8-garbage.key")
+    // deleteOnExit is handled by TempFile
+
+    val privateKeyFile = new PrivateKeyFile(tempFile)
+    val tryPrivateKey = privateKeyFile.readPrivateKey()
+
+    intercept[InvalidKeySpecException] {
+      tryPrivateKey.get()
+    }
+  }
+
+  test("File is RSA Private Key") {
+    val tempFile = TempFile.fromResourcePath("/keys/test-pkcs8.key")
+    // deleteOnExit is handled by TempFile
+
+    val privateKeyFile = new PrivateKeyFile(tempFile)
+    val tryPrivateKey = privateKeyFile.readPrivateKey()
+
+    assert(tryPrivateKey.isReturn)
+    val privateKey = tryPrivateKey.get()
+    assert(privateKey.getFormat == "PKCS#8")
+    assert(privateKey.getAlgorithm == "RSA")
+  }
+
+  test("File is DSA Private Key") {
+    val tempFile = TempFile.fromResourcePath("/keys/test-pkcs8-dsa.key")
+    // deleteOnExit is handled by TempFile
+
+    val privateKeyFile = new PrivateKeyFile(tempFile)
+    val tryPrivateKey = privateKeyFile.readPrivateKey()
+
+    assert(tryPrivateKey.isReturn)
+    val privateKey = tryPrivateKey.get()
+    assert(privateKey.getFormat == "PKCS#8")
+    assert(privateKey.getAlgorithm == "DSA")
+  }
+
+  test("File is EC Private Key") {
+    val tempFile = TempFile.fromResourcePath("/keys/test-pkcs8-ec.key")
+    // deleteOnExit is handled by TempFile
+
+    val privateKeyFile = new PrivateKeyFile(tempFile)
+    val tryPrivateKey = privateKeyFile.readPrivateKey()
+
+    assert(tryPrivateKey.isReturn)
+    val privateKey = tryPrivateKey.get()
+    assert(privateKey.getFormat == "PKCS#8")
+    assert(privateKey.getAlgorithm == "EC")
+  }
+
+}
