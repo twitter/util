27781b43fb5a84b0f4915024798b9c4c0b49e737
Yufan Gong <yufang@twitter.com>
2018-03-15 05:58:27 +0000

    Upgrade libthrift to 0.10.0
    Summary: Problem:
    
    We are sitting on an ancient libthrift version for a long time.
    
    Solution:
    
    Upgrade it.
    
    JIRA Issues: CSL-1406
    
    TBR=true
    
    Differential Revision: https://phabricator.twitter.biz/D124620

--- util-thrift/src/main/scala/com/twitter/util/ThriftCodec.scala
+++ util-thrift/src/main/scala/com/twitter/util/ThriftCodec.scala
@@ -15,13 +15,13 @@ class ThriftCodec[T <: TBase[_, _]: Manifest, P <: TProtocolFactory: Manifest]
   protected lazy val prototype: T =
     manifest[T].runtimeClass.asInstanceOf[Class[T]].newInstance
 
-  override lazy val protocolFactory: TProtocolFactory =
+  lazy val protocolFactory: TProtocolFactory =
     manifest[P].runtimeClass.asInstanceOf[Class[P]].newInstance
 
-  override def encode(item: T): Array[Byte] = toBytes(item)
+  def encode(item: T): Array[Byte] = toBytes(item)
 
-  override def decode(bytes: Array[Byte]): T = {
-    val obj = prototype.deepCopy
+  def decode(bytes: Array[Byte]): T = {
+    val obj: TBase[_, _] = prototype.deepCopy.asInstanceOf[TBase[_, _]]
     fromBytes(obj, bytes)
     obj.asInstanceOf[T]
   }
