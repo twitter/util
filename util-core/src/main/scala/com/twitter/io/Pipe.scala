1d6e539ecd41afb844c9c946b071b367b05c0fd2
Vladimir Kostyukov <vkostyukov@twitter.com>
2018-09-25 21:10:50 +0000

    util-core: Scaladoc for Pipes
    # Problem / Solution
    
    We need better docs for Pipes. Here are the better docs for pipes.
    
    JIRA Issues: CSL-2701
    
    Differential Revision: https://phabricator.twitter.biz/D218115

--- util-core/src/main/scala/com/twitter/io/Pipe.scala
+++ util-core/src/main/scala/com/twitter/io/Pipe.scala
@@ -6,12 +6,70 @@ import com.twitter.util.{Future, Promise, Return, Time}
  * A synchronous in-memory pipe that connects [[Reader]] and [[Writer]] in the sense
  * that a reader's input is the output of a writer.
  *
- * Reads and writes on the pipe are matched one to one and only one outstanding `read`
- * or `write` is permitted in the current implementation. That is, the `write` (its
- * returned [[Future]]) is resolved when the `read` consumes the written data.
+ * A pipe is structured as a smash of both interfaces, a [[Reader]] and a [[Writer]] such that can
+ * be passed directly to a consumer or a producer.
  *
- * It is safe to call `read`, `write`, and `close` concurrently. The individual calls
- * are synchronized on the given [[Pipe]].
+ * {{{
+ *   def consumer(r: Reader[Buf]): Future[Unit] = ???
+ *   def producer(w: Writer[Buf]): Future[Unit] = ???
+ *
+ *   val p = new Pipe[Buf]
+ *
+ *   consumer(p)
+ *   producer(p)
+ * }}}
+ *
+ * Reads and writes on the pipe are matched one to one and only one outstanding `read`
+ * or `write` is permitted in the current implementation (multiple pending writes or reads
+ * resolve into [[IllegalStateException]] while leaving the pipe healthy). That is, the `write`
+ * (its returned [[Future]]) is resolved when the `read` consumes the written data.
+ *
+ * Here is, for example, a very typical write-loop that writes into a pipe-backed [[Writer]]:
+ *
+ * {{{
+ *   def writeLoop(w: Writer[Buf], data: List[Buf]): Future[Unit] = data match {
+ *     case h :: t => p.write(h).before(writeLoop(w, t))
+ *     case Nil => w.close()
+ *   }
+ * }}}
+ *
+ * Reading from a pipe-backed [[Reader]] is no different from working with any other reader:
+ *
+ *{{{
+ *   def readLoop(r: Reader[Buf], process: Buf => Future[Unit]): Future[Unit] = r.read().flatMap {
+ *     case Some(chunk) => process(chunk).before(readLoop(r, process))
+ *     case None => Future.Done
+ *   }
+ * }}}
+ *
+ * == Thread Safety ==
+ *
+ * It is safe to call `read`, `write`, `fail`, `discard`, and `close` concurrently. The individual
+ * calls are synchronized on the given [[Pipe]].
+ *
+ * == Closing or Failing Pipes ==
+ *
+ * Besides expecting a write or a read, a pipe can be closed or failed. A writer can do both `close`
+ * and `fail` the pipe, while reader can only fail the pipe via `discard`.
+ *
+ * The following behavior is expected with regards to reading from or writing into a closed or a
+ * failed pipe:
+ *
+ *  - Writing into a closed pipe yields [[IllegalStateException]]
+ *  - Reading from a closed pipe yields EOF ([[Future.None]])
+ *  - Reading from or writing into a failed pipe yields a failure it was failed with
+ *
+ * It's also worth discussing how pipes are being closed. As closure is always initiated by a
+ * producer (writer), there is a machinery allowing it to be notified when said closure is observed
+ * by a consumer (reader).
+ *
+ * The following rules should help reasoning about closure signals in pipes:
+ *
+ * - Closing a pipe with a pending read resolves said read into EOF and returns a [[Future.Unit]]
+ * - Closing a pipe with a pending write fails said write with [[IllegalStateException]] and
+ *   returns a future that will be satisfied when a consumer observes the closure (EOF) via read
+ * - Closing an idle pipe returns a future that will be satisfied when a consumer observes the
+ *   closure (EOF) via read
  */
 final class Pipe[A <: Buf] extends Reader[A] with Writer[A] {
 
@@ -22,15 +80,15 @@ final class Pipe[A <: Buf] extends Reader[A] with Writer[A] {
 
   def read(n: Int): Future[Option[A]] = synchronized {
     state match {
-      case State.Failing(exc) =>
+      case State.Failed(exc) =>
         Future.exception(exc)
 
       case State.Closing(reof) =>
-        state = State.Eof
+        state = State.Closed
         reof.setDone()
         Future.None
 
-      case State.Eof =>
+      case State.Closed =>
         Future.None
 
       case State.Idle =>
@@ -50,22 +108,19 @@ final class Pipe[A <: Buf] extends Reader[A] with Writer[A] {
         Future.value(Some(buf.slice(0, n).asInstanceOf[A]))
 
       case State.Reading(_, _) =>
-        Future.exception(new IllegalStateException("read() while Reading"))
+        Future.exception(new IllegalStateException("read() while read is pending"))
     }
   }
 
-  def discard(): Unit = synchronized {
-    val cause = new Reader.ReaderDiscarded()
-    fail(cause)
-  }
+  def discard(): Unit = fail(new Reader.ReaderDiscarded())
 
   def write(buf: A): Future[Unit] = synchronized {
     state match {
-      case State.Failing(exc) =>
+      case State.Failed(exc) =>
         Future.exception(exc)
 
-      case State.Eof | State.Closing(_) =>
-        Future.exception(new IllegalStateException("write after close"))
+      case State.Closed | State.Closing(_) =>
+        Future.exception(new IllegalStateException("write() while closed"))
 
       case State.Idle =>
         val p = new Promise[Unit]()
@@ -86,37 +141,34 @@ final class Pipe[A <: Buf] extends Reader[A] with Writer[A] {
         Future.Done
 
       case State.Writing(_, _) =>
-        Future.exception(new IllegalStateException("write while Writing"))
+        Future.exception(new IllegalStateException("write() while write is pending"))
     }
   }
 
   def fail(cause: Throwable): Unit = synchronized {
-    // We fix the state before inspecting it so we enter in a
-    // good state if setting promises recurses.
-    val oldState = state
-    oldState match {
-      case State.Eof | State.Failing(_) =>
+    state match {
+      case State.Closed | State.Failed(_) =>
       // do not update state to failing
       case State.Idle =>
-        state = State.Failing(cause)
+        state = State.Failed(cause)
       case State.Closing(reof) =>
-        state = State.Failing(cause)
+        state = State.Failed(cause)
         reof.setException(cause)
       case State.Reading(_, p) =>
-        state = State.Failing(cause)
+        state = State.Failed(cause)
         p.setException(cause)
       case State.Writing(_, p) =>
-        state = State.Failing(cause)
+        state = State.Failed(cause)
         p.setException(cause)
     }
   }
 
   def close(deadline: Time): Future[Unit] = synchronized {
     state match {
-      case State.Failing(t) =>
+      case State.Failed(t) =>
         Future.exception(t)
 
-      case State.Eof =>
+      case State.Closed =>
         Future.Done
 
       case State.Closing(p) =>
@@ -128,14 +180,14 @@ final class Pipe[A <: Buf] extends Reader[A] with Writer[A] {
         reof
 
       case State.Reading(_, p) =>
-        state = State.Eof
+        state = State.Closed
         p.update(Return.None)
         Future.Done
 
       case State.Writing(_, p) =>
         val reof = new Promise[Unit]()
         state = State.Closing(reof)
-        p.setException(new IllegalStateException("close while write is pending"))
+        p.setException(new IllegalStateException("close() while write is pending"))
         reof
     }
   }
@@ -168,8 +220,8 @@ object Pipe {
      */
     final case class Writing[A <: Buf](buf: A, p: Promise[Unit]) extends State[A]
 
-    /** Indicates this was `fail`-ed or `discard`-ed. */
-    final case class Failing(exc: Throwable) extends State[Nothing]
+    /** Indicates the pipe was failed. */
+    final case class Failed(exc: Throwable) extends State[Nothing]
 
     /**
      * Indicates a close occurred while `Idle` — no reads or writes were pending.
@@ -179,6 +231,6 @@ object Pipe {
     final case class Closing(reof: Promise[Unit]) extends State[Nothing]
 
     /** Indicates the reader has seen the EOF. No more reads or writes are allowed. */
-    case object Eof extends State[Nothing]
+    case object Closed extends State[Nothing]
   }
 }
