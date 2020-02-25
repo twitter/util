0d3ab17f250a5caf4faf63b458ed2d640009cc71
muachilin <32566798+muachilin@users.noreply.github.com>
2020-02-25 11:42:05 +0100

    Add more tests for Cancellable

--- util-core/src/test/scala/com/twitter/util/CancellableTest.scala
+++ util-core/src/test/scala/com/twitter/util/CancellableTest.scala
@@ -9,10 +9,23 @@ class CancellableTest extends WordSpec {
       def increment: Unit = count += 1
       val s = new CancellableSink(increment)
       s.cancel()
+      assert(s.isCancelled)
       assert(count == 1)
       s.cancel()
+      assert(s.isCancelled)
       assert(count == 1)
-      assert(!"Hi Meng-Jin!".isEmpty)
+    }
+    "linking not supported" in {
+      var count = 1
+      def multiply: Unit = count *= 2
+      val s1 = new CancellableSink(multiply)
+      val s2 = new CancellableSink(multiply)
+      assertThrows[Exception] {
+        s1.linkTo(s2)
+      }
+      assertThrows[Exception] {
+        s2.linkTo(s1)
+      }
     }
   }
 }
