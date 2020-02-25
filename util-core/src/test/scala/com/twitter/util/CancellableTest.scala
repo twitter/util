549fda97397ff3536d4a723b9cf13c558758b937
muachilin <32566798+muachilin@users.noreply.github.com>
2020-02-25 14:28:55 +0100

    Add test for abstract class of Cancellable

--- util-core/src/test/scala/com/twitter/util/CancellableTest.scala
+++ util-core/src/test/scala/com/twitter/util/CancellableTest.scala
@@ -3,17 +3,43 @@ package com.twitter.util
 import org.scalatest.WordSpec
 
 class CancellableTest extends WordSpec {
+  "Cancellable" should {
+    "cancel test" in {
+      Boolean cancelMethodCalled = false
+      Boolean linkToMethodCalled = false
+      val s = new class CancellableObjectTest extends Cancellable {
+        def cancel(): Unit = { 
+          cancelMethodCalled = true
+        }
+        def linkTo(other: CancellableObjectTest): Unit = {
+          linkToMethodCalled = true
+        }
+      }
+      assert(!s.isCancelled)
+      assert(!cancelMethodCalled)
+      s.cancel()
+      assert(cancelMethodCalled)
+      assert(!linkToMethodCalled)
+      s.linkTo(s)
+      assert(linkToMethodCalled)
+    }
+  }
   "CancellableSink" should {
     "cancel once" in {
       var count = 0
       def increment: Unit = count += 1
       val s = new CancellableSink(increment)
       s.cancel()
-      assert(s.isCancelled)
       assert(count == 1)
       s.cancel()
-      assert(s.isCancelled)
       assert(count == 1)
+    }
+    "confirm cancelled" in {
+      var count = 0
+      def increment: Unit = count += 1
+      val s = new CancellableSink(increment)
+      s.cancel()
+      assert(s.isCancelled)
     }
     "linking not supported" in {
       var count = 1
