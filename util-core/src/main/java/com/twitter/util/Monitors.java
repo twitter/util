2430cbc43db33fdcb01a14dee1076da68ecb9d2e
Ryan O'Neill <ryano@twitter.com>
2020-07-22 01:47:20 +0000

    util-core: Rework Monitors Internals for Unidoc Generation
    Problem / Solution
    
    Trying to generate `unidoc` documentation for util fails when using
    Scala 2.12.11. This is due to it not liking the `it` value of `Monitors`
    which has type `Monitor$`. To work around the problem, we stepped the
    version of Scala that we use go generate documentation down to 2.12.8.
    However, it's not necessary for `Monitors` to have this value at all,
    and so removing it is in our best interests moving forward.
    
    JIRA Issues: CSL-9977
    
    Differential Revision: https://phabricator.twitter.biz/D520046

--- util-core/src/main/java/com/twitter/util/Monitors.java
+++ util-core/src/main/java/com/twitter/util/Monitors.java
@@ -14,56 +14,53 @@ import static com.twitter.util.Function.func0;
 public final class Monitors {
   private Monitors() { }
 
-  private static final Monitor$ it =
-      Monitor$.MODULE$;
-
   /**
    * The singleton `Monitor` instance that is the Monitor companion object.
    */
   public static Monitor instance() {
-    return it;
+    return Monitor$.MODULE$;
   }
 
   /**
    * Equivalent to calling `Monitor.get`.
    */
   public static Monitor get() {
-    return it.get();
+    return Monitor$.MODULE$.get();
   }
 
   /**
    * Equivalent to calling `Monitor.set`.
    */
   public static void set(Monitor monitor) {
-    it.set(monitor);
+    Monitor$.MODULE$.set(monitor);
   }
 
   /**
    * Equivalent to calling `Monitor.using`.
    */
   public static <T> T using(Monitor monitor, Supplier<T> supplier) {
-    return it.using(monitor, func0(supplier));
+    return Monitor$.MODULE$.using(monitor, func0(supplier));
   }
 
   /**
    * Equivalent to calling `Monitor.restoring`.
    */
   public static <T> T restoring(Supplier<T> supplier) {
-    return it.restoring(func0(supplier));
+    return Monitor$.MODULE$.restoring(func0(supplier));
   }
 
   /**
    * Equivalent to `Monitor.catcher`.
    */
   public static PartialFunction<Throwable, BoxedUnit> catcher() {
-    return it.catcher();
+    return Monitor$.MODULE$.catcher();
   }
 
   /**
    * Equivalent to calling `Monitor.isActive`.
    */
   public static boolean isActive() {
-    return it.isActive();
+    return Monitor$.MODULE$.isActive();
   }
 
 }
