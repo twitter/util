d22f657cad2db0b663f3155496bb3ce671256fdd
Moses Nakamura <mnakamura@twitter.com>
2021-06-09 19:24:36 +0000

    util-validator: Unmangling more names in ScalaValidator
    Problem
    
    When merging in Felix's upgrade patch, I broke a unit test by trying
    to unmangle in a too-narrow way, and missed it through negligence.
    
    Solution
    
    Unmangle appropriately, the way the original commit worked!
    
    JIRA Issues: CSL-11030
    
    Differential Revision: https://phabricator.twitter.biz/D684906

--- util-validator/src/main/scala/com/twitter/util/validation/ScalaValidator.scala
+++ util-validator/src/main/scala/com/twitter/util/validation/ScalaValidator.scala
@@ -860,15 +860,15 @@ class ScalaValidator private[validation] (
     val results = new mutable.ListBuffer[ConstraintViolation[T]]()
     val parameters: Array[Parameter] = executableDescriptor.executable.getParameters
     val parameterNamesList: Array[String] =
-      parameterNames match {
+      (parameterNames match {
         case Some(names) =>
           names
         case _ =>
-          getExecutableParameterNames(executableDescriptor.executable).map { maybeMangled =>
-            DescriptorFactory.unmangleName(
-              maybeMangled
-            ) // parameter names are encoded since Scala 2.13.5
-          }
+          getExecutableParameterNames(executableDescriptor.executable)
+      }).map { maybeMangled =>
+        DescriptorFactory.unmangleName(
+          maybeMangled
+        ) // parameter names are encoded since Scala 2.13.5
       }
 
     // executable parameter constraints
