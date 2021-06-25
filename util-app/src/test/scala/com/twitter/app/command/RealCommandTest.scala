981e622a55dd4365f78472ed6435699d47e90337
Sameer Brenn <sbrenn@twitter.com>
2021-06-25 03:15:01 +0000

    util-app: Fix RealCommandTest when run via sbt
    Problem: RealCommandTest does not work when run via sbt because the current
    working directory is different than when the test is run via pants.
    
    Solution: Use the path to the resource in the classloader to find the test
    script rather than making an assumption about where it is.
    
    JIRA Issues: CSL-11042
    
    Differential Revision: https://phabricator.twitter.biz/D694390

--- util-app/src/test/scala/com/twitter/app/command/RealCommandTest.scala
+++ util-app/src/test/scala/com/twitter/app/command/RealCommandTest.scala
@@ -1,11 +1,15 @@
 package com.twitter.app.command
 
+import com.twitter.app.command.RealCommandTest.ScriptPath
 import com.twitter.conversions.DurationOps._
 import com.twitter.io.{Buf, Reader}
 import com.twitter.util.{Await, Awaitable, Future}
-import java.io.File
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+
+object RealCommandTest {
+  private val ScriptPath = getClass.getClassLoader.getResource("command-test.sh").getPath
+}
 
 class RealCommandTest extends AnyFunSuite with Matchers {
   private def await[A](awaitable: Awaitable[A]): A = Await.result(awaitable, 5.seconds)
@@ -16,13 +20,13 @@ class RealCommandTest extends AnyFunSuite with Matchers {
   }
 
   test("Executes a script and gets output") {
-    val output =
-      Command.run(
-        Seq("./command-test.sh", "10", "0.1", "0"),
-        Some(new File("util/util-app/src/test/resources")),
-        Map(
-          "EXTRA_ENV" -> "test value"
-        ))
+    val output = Command.run(
+      Seq(ScriptPath, "10", "0.1", "0"),
+      None,
+      Map(
+        "EXTRA_ENV" -> "test value"
+      ))
+
     val firstLine = await(output.stdout.read())
     val firstError = await(output.stderr.read())
     firstLine.map(parseUtf8Buf) shouldBe Some("Stdout # 1 test value")
@@ -38,8 +42,8 @@ class RealCommandTest extends AnyFunSuite with Matchers {
   test("Executes a script and gets failure") {
     val output =
       Command.run(
-        Seq("./command-test.sh", "10", "0.1", "1"),
-        Some(new File("util/util-app/src/test/resources")),
+        Seq(ScriptPath, "10", "0.1", "1"),
+        None,
         Map(
           "EXTRA_ENV" -> "test value"
         ))
