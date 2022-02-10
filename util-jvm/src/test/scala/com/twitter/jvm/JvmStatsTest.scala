120f892c2227b3934317fc0c0d62afb1222034d5
Jing Yan <jyan@twitter.com>
2022-02-10 23:09:23 +0000

    util-jvm: fix flaky JvmStatsTest
    Problem
    `JvmStatsTest` is flaky since it depends on the JVM and
    GC version the test is running on. It passed in source but
    failed OSS due to running in a different environment with
    error message:
    
    ```
    2022-02-07T21:22:36.8995460Z [info] JvmStatsTest:
    2022-02-07T21:22:36.8996550Z [info] - expressions are instrumented
    2022-02-07T21:22:36.9456672Z [info] - memory_pool expression is registered *** FAILED ***
    2022-02-07T21:22:36.9491043Z [info]   labels.exists(((x$6: Map[String,String]) => x$6.get(key).get.==(value))) was false (JvmStatsTest.scala:81)
    2022-02-07T21:22:36.9517859Z [info] - file_descriptors expression is registered
    2022-02-07T21:22:36.9578658Z [info] - gc_cycles expression is registered *** FAILED ***
    2022-02-07T21:22:36.9582297Z [info]   Set(Map("role" -> "jvm", "gc_pool" -> "ParNew"), Map("role" -> "jvm", "gc_pool" -> "ConcurrentMarkSweep"), Map("role" -> "jvm")) did not contain Map("role" -> "jvm", "gc_pool" -> "PS_Scavenge") (JvmStatsTest.scala:51)
    2022-02-07T21:22:36.9611928Z [info] - gc_latency expression is registered *** FAILED ***
    2022-02-07T21:22:36.9615667Z [info]   Set(Map("role" -> "jvm", "gc_pool" -> "ConcurrentMarkSweep"), Map("role" -> "jvm"), Map("role" -> "jvm", "gc_pool" -> "ParNew")) did not contain Map("role" -> "jvm", "gc_pool" -> "PS_Scavenge") (JvmStatsTest.scala:60)
    ```
    
    Solution
    Remove the assumptions in the tests that varies per
    JVM/GC implementation.
    
    JIRA Issues: CSL-11698
    
    Differential Revision: https://phabricator.twitter.biz/D829391

--- util-jvm/src/test/scala/com/twitter/jvm/JvmStatsTest.scala
+++ util-jvm/src/test/scala/com/twitter/jvm/JvmStatsTest.scala
@@ -18,65 +18,17 @@ class JvmStatsTest extends AnyFunSuite {
     }
   }
 
-  test("memory_pool expression is registered") {
-    val memoryPoolExpressionLabels = expressions
-      .filter {
-        case (k, _) => k.name == "memory_pool"
-      }.keySet.map(_.labels)
-
-    assertLabels(memoryPoolExpressionLabels, "role", "jvm")
-    assertLabels(memoryPoolExpressionLabels, "kind", "Code_Cache")
-    assertLabels(memoryPoolExpressionLabels, "kind", "Compressed_Class_Space")
-    assertLabels(memoryPoolExpressionLabels, "kind", "Metaspace")
-    assertLabels(memoryPoolExpressionLabels, "kind", "PS_Eden_Space")
-    assertLabels(memoryPoolExpressionLabels, "kind", "Heap")
-    assertLabels(memoryPoolExpressionLabels, "kind", "PS_Survivor_Space")
-    assertLabels(memoryPoolExpressionLabels, "kind", "PS_Old_Gen")
-  }
-
-  test("file_descriptors expression is registered") {
-    val fileDescriptorsExpressionLabels = expressions
-      .filter {
-        case (k, _) => k.name == "file_descriptors"
-      }.keySet.map(_.labels)
-
-    assertLabels(fileDescriptorsExpressionLabels, "role", "jvm")
-  }
-
-  test("gc_cycles expression is registered") {
-    val gcCyclesExpressionLabels = expressions
-      .filter {
-        case (k, _) => k.name == "gc_cycles"
-      }.keySet.map(_.labels)
-    assert(gcCyclesExpressionLabels.contains(Map("role" -> "jvm", "gc_pool" -> "PS_Scavenge")))
-    assert(gcCyclesExpressionLabels.contains(Map("role" -> "jvm", "gc_pool" -> "PS_MarkSweep")))
-  }
-
-  test("gc_latency expression is registered") {
-    val gcLatenciesExpressionLabels = expressions
-      .filter {
-        case (k, _) => k.name == "gc_latency"
-      }.keySet.map(_.labels)
-    assert(gcLatenciesExpressionLabels.contains(Map("role" -> "jvm", "gc_pool" -> "PS_Scavenge")))
-    assert(gcLatenciesExpressionLabels.contains(Map("role" -> "jvm", "gc_pool" -> "PS_MarkSweep")))
-  }
-
-  test("jvm_uptime expression is registered") {
-    val jvmUptimeExpressionLabels = expressions
-      .filter {
-        case (k, _) => k.name == "jvm_uptime"
-      }.keySet.map(_.labels)
-
-    assertLabels(jvmUptimeExpressionLabels, "role", "jvm")
+  test("expressions are registered") {
+    assertExpressionLabels("memory_pool")
+    assertExpressionLabels("file_descriptors")
+    assertExpressionLabels("gc_cycles")
+    assertExpressionLabels("gc_latency")
+    assertExpressionLabels("jvm_uptime")
   }
 
   private[this] def assertMetric(expressionSchema: ExpressionSchema): Unit =
     assert(expressionSchema.expr.isInstanceOf[MetricExpression])
 
-  private[this] def assertLabels(
-    labels: scala.collection.Set[Map[String, String]],
-    key: String,
-    value: String
-  ): Unit =
-    assert(labels.exists(_.get(key).get == value))
+  private[this] def assertExpressionLabels(expressionKey: String): Unit =
+    assert(expressions.keysIterator.exists(_.name == expressionKey))
 }
